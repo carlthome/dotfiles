3d22fc0d43ebbedb7a04d36bd2c72f3fab075564
Carl Thomé <carl.thome@xlnaudio.com>
2022-07-21 20:54:32 +0200

    Remove pyopenssl using packages & add starship

--- shell.nix
+++ shell.nix
@@ -5,32 +5,37 @@ let
     pip
     setuptools
     ipython
-    black
-    mypy
+    #black
+    #mypy
     flake8
     isort
     numpy
     scipy
     pandas
-    matplotlib
-    librosa
-    pytorch
-    tensorflow
+    #matplotlib
+    #librosa
+    #pytorch
+    #tensorflow
+    #jax
+    #jaxlib
   ]);
 in
 mkShell {
   buildInputs = [
+    rustup
+    cargo
+    starship
     python
     nixpkgs-fmt
     poetry
-    pre-commit
+    #pre-commit
     git
     act
     ffmpeg
     libsndfile
     sox
-    cudatoolkit
-    cudaPackages.cudnn
+    google-cloud-sdk
+    github-cli
   ];
   shellHook = ''
     echo "Hello $(whoami)!"
