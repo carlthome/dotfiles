e6bc0e0b30cdbe110a7b62920b2a95a8cf5e0de7
Carl Thomé <carl.thome@xlnaudio.com>
2022-07-22 11:38:49 +0200

    Inline shell.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,70 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        packages.hello = pkgs.hello;
+        defaultPackage = packages.hello;
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
-  };
+        # TODO Let shell.nix still be around for nix-shell legacy usage.
+        #devShells.default = import ./shell.nix { inherit pkgs; };
+        devShells.default = with pkgs;
+          let
+            python = python3.withPackages (p: with p; [
+              #black
+              #jax
+              #jaxlib
+              #librosa
+              #matplotlib
+              #mypy
+              #pytorch
+              #tensorflow
+              flake8
+              ipython
+              isort
+              numpy
+              opencv
+              pandas
+              pip
+              scipy
+              setuptools
+            ]);
+          in
+          pkgs.mkShell {
+            buildInputs = [
+              #pre-commit
+              act
+              awscli
+              cargo
+              ffmpeg
+              git
+              github-cli
+              google-cloud-sdk
+              jupyter
+              jq
+              libsndfile
+              nixpkgs-fmt
+              nodejs
+              nodePackages.npm
+              nodePackages.prettier
+              opencv
+              poetry
+              python
+              pdfgrep
+              rustup
+              sox
+              starship
+              shellcheck
+              vim
+            ];
+            shellHook = ''
+              echo "Hello $(whoami)!"
+            '';
+          };
+      }
+    );
 }
