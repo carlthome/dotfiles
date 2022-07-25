ab08133b58240b3f39f06010d6c2f9a93ebd0268
Carl Thomé <carl.thome@xlnaudio.com>
2022-07-25 22:19:54 +0200

    Don't inline shell.nix

--- flake.nix
+++ flake.nix
@@ -12,62 +12,7 @@
       rec {
         packages.hello = pkgs.hello;
         packages.default = packages.hello;
-
-        # TODO Let shell.nix still be around for nix-shell legacy usage.
-        #devShells.default = import ./shell.nix { inherit pkgs; };
-        devShells.default = with pkgs;
-          let
-            python = python3.withPackages (p: with p; [
-              #black
-              #jax
-              #jaxlib
-              #librosa
-              #matplotlib
-              #mypy
-              #pytorch
-              #tensorflow
-              flake8
-              ipython
-              isort
-              numpy
-              opencv
-              #pandas
-              pip
-              scipy
-              setuptools
-            ]);
-          in
-          pkgs.mkShell {
-            buildInputs = [
-              #pre-commit
-              act
-              awscli
-              cargo
-              ffmpeg
-              git
-              github-cli
-              google-cloud-sdk
-              jupyter
-              jq
-              libsndfile
-              nixpkgs-fmt
-              nodejs
-              nodePackages.npm
-              nodePackages.prettier
-              opencv
-              poetry
-              python
-              pdfgrep
-              rustup
-              sox
-              starship
-              shellcheck
-              vim
-            ];
-            shellHook = ''
-              echo "Hello $(whoami)!"
-            '';
-          };
+        devShells.default = import ./shell.nix { inherit pkgs; };
       }
     );
 }
