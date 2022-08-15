762cf5b8c8b2a7db15b05deb4409608e138aa457
Carl Thomé <carlthome@gmail.com>
2022-08-15 23:25:59 +0200

    Add some initial GUI programs

--- home-configurations/desktop.nix
+++ home-configurations/desktop.nix
@@ -3,9 +3,19 @@
   home.homeDirectory = "/home/carl";
 
   # TODO Configure home desktop programs when I find the time.
-  home.packages = with pkgs; [
-    #reaper
-    #tdesktop
-    #steam
-  ];
+  home.packages = with pkgs;
+    let python = import ./python.nix { inherit pkgs; };
+    in [
+      reaper
+      tdesktop
+      steam
+      python.env
+      jupyter
+      keepassxc
+      signal-desktop
+    ];
+
+  programs.firefox.enable = true;
+  programs.chromium.enable = true;
+  programs.obs-studio.enable = true;
 }
