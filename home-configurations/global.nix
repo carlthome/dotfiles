c089e8fa3ee48fb0d65cbee07b705614ab472f3d
Carl Thomé <carlthome@gmail.com>
2022-09-13 00:10:07 +0200

    Run nix fmt

--- home-configurations/global.nix
+++ home-configurations/global.nix
@@ -4,95 +4,94 @@
     allowUnfreePredicate = (pkg: true);
   };
 
-  home =
-    {
-      stateVersion = "22.05";
-      enableNixpkgsReleaseCheck = true;
-      packages = with pkgs; [
-        act
-        awscli
-        black
-        cachix
-        cargo
-        cmake
-        coreutils
-        curl
-        dive
-        docker
-        docker-compose
-        fantasque-sans-mono
-        ffmpeg-full
-        git
-        google-cloud-sdk
-        jq
-        jupyter
-        kind
-        libsndfile
-        nixpkgs-fmt
-        nodejs
-        nodePackages.npm
-        nodePackages.prettier
-        opencv
-        pdfgrep
-        poetry
-        postman
-        pre-commit
-        rclone
-        rsync
-        rustc
-        shellcheck
-        slack
-        sox
-        sqlitebrowser
-        tree
-        wget
-      ];
-      sessionVariables = {
-        DOCKER_BUILDKIT = true;
-      };
-      shellAliases = {
-        d = "docker run --rm -it";
-        k = "kubectl";
-        g = "gcloud beta interactive";
-      };
+  home = {
+    stateVersion = "22.05";
+    enableNixpkgsReleaseCheck = true;
+    packages = with pkgs; [
+      act
+      awscli
+      black
+      cachix
+      cargo
+      cmake
+      coreutils
+      curl
+      dive
+      docker
+      docker-compose
+      fantasque-sans-mono
+      ffmpeg-full
+      git
+      google-cloud-sdk
+      jq
+      jupyter
+      kind
+      libsndfile
+      nixpkgs-fmt
+      nodejs
+      nodePackages.npm
+      nodePackages.prettier
+      opencv
+      pdfgrep
+      pipenv
+      python3
+      python3Packages.pip
+      poetry
+      postman
+      pre-commit
+      rclone
+      rsync
+      rustc
+      shellcheck
+      slack
+      sox
+      sqlitebrowser
+      tree
+      wget
+    ];
+    sessionVariables = { DOCKER_BUILDKIT = true; };
+    shellAliases = {
+      d = "docker run --rm -it";
+      k = "kubectl";
+      g = "gcloud beta interactive";
     };
+  };
 
   fonts.fontconfig.enable = true;
 
-  programs =
-    {
-      vim.enable = true;
-      fish.enable = true;
-      zsh.enable = true;
-      nushell.enable = true;
-      bash = {
-        enable = true;
-        initExtra = ''
-          eval "$(pdm --pep582)"
-        '';
-      };
-      tmux.enable = true;
-      starship = {
-        enable = true;
-        enableZshIntegration = true;
-        enableBashIntegration = true;
-        enableFishIntegration = true;
-      };
-      direnv = {
-        enable = true;
-        nix-direnv.enable = true;
-      };
-      git = {
-        enable = true;
-        userName = "Carl Thomé";
-        userEmail = "carlthome@gmail.com";
-        # TODO Play around with non-standard git diff extensions.
-        delta.enable = false;
-        diff-so-fancy.enable = false;
-        difftastic.enable = false;
-      };
-      gitui.enable = true;
-      gh.enable = true;
-      vscode = import ./vscode.nix { inherit pkgs; };
+  programs = {
+    vim.enable = true;
+    fish.enable = true;
+    zsh.enable = true;
+    nushell.enable = true;
+    bash = {
+      enable = true;
+      initExtra = ''
+        eval "$(pdm --pep582)"
+      '';
     };
+    tmux.enable = true;
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+    };
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    git = {
+      enable = true;
+      userName = "Carl Thomé";
+      userEmail = "carlthome@gmail.com";
+      # TODO Play around with non-standard git diff extensions.
+      delta.enable = false;
+      diff-so-fancy.enable = false;
+      difftastic.enable = false;
+    };
+    gitui.enable = true;
+    gh.enable = true;
+    vscode = import ./vscode.nix { inherit pkgs; };
+  };
 }
