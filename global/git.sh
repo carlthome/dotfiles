57ff75397e1585ad1e181032d1896f2e98b11637
Carl Thomé <carlthome@gmail.com>
2022-09-13 00:10:05 +0200

    Fix git-refresh function and move it into git.sh

--- global/git.sh
+++ global/git.sh
@@ -1,3 +1,28 @@
+# Download remote changes for all git repos and check if local repo is up-to-date.
+function git-refresh {
+  oldpwd=$(pwd)
+  git_root="$HOME/git"
+
+  # Find local repos.
+  repos=$(find "$git_root" -name .git | xargs dirname)
+
+  # Go through every repo, fetch remote changes and warn on local changes not found remotely.
+  for repo in $repos; do
+    pushd $repo
+
+    # Download remote changes.
+    git fetch
+
+    # Display local changes.
+    status=$(git status --porcelain)
+    if [[ -n $status ]]; then
+      printf "%s:\n%s\n\n" "$repo" "$status"
+    fi;
+
+    popd
+  done
+}
+
 # Show current git branch on terminal prompt.
 PS1='($(git branch --show-current): $(git log --oneline -n 1)) '"${PS1}"
 export PS1
