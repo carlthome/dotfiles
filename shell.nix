ff0462fa88bedf6281ea90c6fe501e0522766819
Carl Thomé <carlthome@gmail.com>
2022-09-13 00:10:06 +0200

    Add initial shell.nix (still playing around)

---
+++ shell.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> {} }:
+  let
+    python = pkgs.python3.withPackages (p: with p; [
+      pip
+      setuptools
+      ipython
+      black
+      mypy
+      flake8
+      isort
+      numpy
+      scipy
+      pandas
+      matplotlib
+      librosa
+      pytorchWithCuda
+      tensorflowWithCuda
+    ]);
+  in
+  pkgs.mkShell {
+    buildInputs = [
+      python
+      pkgs.git
+      pkgs.ffmpeg
+      pkgs.libsndfile
+      pkgs.sox
+      pkgs.cudatoolkit
+      pkgs.cudaPackages.cudnn
+    ];
+    shellHook = ''
+      PYTHONPATH=${python}/${python.sitePackages}
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.cudaPackages_10_1.cudatoolkit}/lib:${pkgs.cudaPackages_10_1.cudnn}/lib:${pkgs.cudaPackages_10_1.cudatoolkit.lib}/lib
+      alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' TMPDIR='$HOME' \pip"
+      export PATH="$(pwd)/_build/pip_packages/bin:$PATH"
+      unset SOURCE_DATE_EPOCH
+    '';
+  }
