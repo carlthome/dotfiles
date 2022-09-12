ac813aaf5389164f382df2dad2a95982fb64d1b6
Carl Thomé <carlthome@gmail.com>
2022-09-13 00:10:07 +0200

    nixfmt

--- home-configurations/python.nix
+++ home-configurations/python.nix
@@ -1,21 +1,22 @@
 { pkgs, ... }: {
-  python = pkgs.python3.withPackages (ps: with ps; [
-    black
-    jax
-    jaxlib
-    librosa
-    matplotlib
-    mypy
-    pytorch
-    tensorflow
-    flake8
-    ipython
-    isort
-    numpy
-    opencv
-    pandas
-    pip
-    scipy
-    setuptools
-  ]);
+  python = pkgs.python3.withPackages (ps:
+    with ps; [
+      black
+      jax
+      jaxlib
+      librosa
+      matplotlib
+      mypy
+      pytorch
+      tensorflow
+      flake8
+      ipython
+      isort
+      numpy
+      opencv
+      pandas
+      pip
+      scipy
+      setuptools
+    ]);
 }
