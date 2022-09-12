a9f610709fc20399c08cca9fff16f0e2cc79f9d7
Carl Thomé <carlthome@gmail.com>
2022-09-13 00:10:05 +0200

    Simplify GCP config prompt

--- global/gcloud.sh
+++ global/gcloud.sh
@@ -1,4 +1,4 @@
-# Paths to Google Cloud SDK and the user's config.
+# Paths to Google Cloud SDK and its config.
 gcloud="$HOME/.cache/cloud-code/installer/google-cloud-sdk"
 config="$HOME/.config/gcloud"
 
@@ -8,27 +8,6 @@ source "$gcloud/path.bash.inc"
 # The next line enables shell command completion for gcloud.
 source "$gcloud/completion.bash.inc"
 
-# Append active gcloud config in Bash terminal prompt. For configuration names
-# containing "prod", the prompt is colored red for extra visibility.
-function gcloud_prompt {
+# Append active gcloud config in Bash terminal prompt.
+export PS1="[$(cat $config/active_config)] ${PS1}"
 
-  # Store original prompt.
-  if [[ "${ORIG_PS1}" == "" ]]; then
-    export ORIG_PS1=$PS1
-  fi
-
-  # Lookup active gcloud config. We look at the internal state directly to avoid starting gcloud on every terminal prompt.
-  active=$(cat $config/active_config)
-
-  # Color prompt based on gcloud config name.
-  if [[ "${active}" == *'prod'* ]]; then
-    active="\e[0;31m[${active}]\e[m "
-  else
-    active="[${active}] "
-  fi
-
-  # Set prompt.
-  export PS1="${ORIG_PS1}${active}"
-}
-
-export PROMPT_COMMAND=gcloud_prompt
