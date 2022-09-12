635277c53b9d3ce689c7da00a56d0a5dc6a9e780
Carl Thomé <carlthome@gmail.com>
2022-09-13 00:10:05 +0200

    Update comment

--- global/nvm.sh
+++ global/nvm.sh
@@ -1,4 +1,4 @@
-# Node version manager
+# Add Node version manager to PATH.
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
