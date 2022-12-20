94d10095742e3222283bdd2cc5707e8b1daa0aac
Carl Thomé <carlthome@gmail.com>
2022-12-20 18:23:33 +0100

    Add wine and chat apps

--- homes/modules/linux.nix
+++ homes/modules/linux.nix
@@ -1,8 +1,15 @@
 { pkgs, ... }: {
+
+  # TODO Set this if not NixOS but still Linux.  
+  # targets.genericLinux.enable = true;
+
   home.packages = with pkgs; [
+    # TODO Stop using flatpak version.
+    #discord
     caprine-bin
     chromium
     dropbox-cli
+    element-desktop
     firefox
     google-chrome
     keepassxc
@@ -11,10 +18,12 @@
     okular
     reaper
     signal-desktop
+    slack
+    spotify
     steam
     tdesktop
+    wineWowPackages.staging
     yabridge
     yabridgectl
-    marker
   ];
 }
