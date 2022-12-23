6f7f45f037421aab00e939e59dd5f750b2c03e08
Carl Thomé <carlthome@gmail.com>
2022-12-23 16:12:41 +0100

    shellcheck + minor tweaks

--- scripts/git-refresh.sh
+++ scripts/git-refresh.sh
@@ -2,23 +2,28 @@
 set -e
 
 # Download remote changes for all git repos and check if local repo is up-to-date.
-git_root="$HOME/git"
+git_root="$HOME/Repos"
 
 # Find local repos.
-repos=$(find "$git_root" -0 -name .git | xargs dirname)
+repos=$(find "$git_root" -print0 -name .git | xargs dirname)
+
+printf "Will run 'git fetch' for:\n%s\n\n" "$repos"
 
 # Go through every repo, fetch remote changes and warn on local changes not found remotely.
 for repo in $repos; do
-  pushd "$repo" || return
+  cwd=$(pwd)
+  cd "$repo"
 
   # Download remote changes.
   git fetch
 
   # Display local changes.
   status=$(git status --porcelain)
-  if [[ -n $status ]]; then
-    printf "%s:\n%s\n\n" "$repo" "$status"
-  fi;
+  if [ -n "$status" ]; then
+    printf "%s has local changes:\n%s\n\n" "$repo" "$status"
+  else
+    printf "%s is up-to-date with remote\n\n" "$repo"
+  fi
 
-  popd || return
+  cd "$cwd"
 done
--- scripts/paper.sh
+++ scripts/paper.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-# List PDFs containing phrase. Can be run as `paper constant-q transform`.
+# List PDFs containing phrase.
 query=$*
-path=$HOME/Documents/Papers
+path=$HOME/Documents
 
 echo "Searching for \"$query\"..."
 pdfgrep --ignore-case --count --recursive "$query" "$path" | sed "/:0/d"
