819b763ee1246fbfc241f787a98a5221eabbac62
Carl Thomé <carlthome@gmail.com>
2023-01-10 22:22:21 +0100

    Switch to vscodium

--- modules/global.nix
+++ modules/global.nix
@@ -105,61 +105,69 @@
     vscode = {
       enable = true;
       package = pkgs.vscodium;
-      #extensions = with pkgs.vscode-extensions; [
-      #  bbenoist.nix
-      #  davidanson.vscode-markdownlint
-      #  #github.copilot
-      #  github.github-vscode-theme
-      #  github.vscode-pull-request-github
-      #  gitlab.gitlab-workflow
-      #  hashicorp.terraform
-      #  jnoortheen.nix-ide
-      #  ms-azuretools.vscode-docker
-      #  ms-kubernetes-tools.vscode-kubernetes-tools
-      #  #ms-python.vscode-pylance
-      #  ms-toolsai.jupyter
-      #  #ms-vscode-remote.remote-ssh
-      #  njpwerner.autodocstring
-      #  pkief.material-icon-theme
-      #  redhat.java
-      #  redhat.vscode-yaml
-      #  svelte.svelte-vscode
-      #];
-      #userSettings = {
-      #  "[python]" = { "editor.defaultFormatter" = "ms-python.black-formatter"; };
-      #  "[yaml]" = { "editor.defaultFormatter" = "redhat.vscode-yaml"; };
-      #  "cloudcode.enableTelemetry" = false;
-      #  "cmake.configureOnOpen" = true;
-      #  "debug.allowBreakpointsEverywhere" = true;
-      #  "editor.fontFamily" = "Fantasque Sans Mono";
-      #  "editor.fontLigatures" = true;
-      #  "editor.fontSize" = 14;
-      #  "editor.formatOnSave" = true;
-      #  "editor.inlineSuggest.enabled" = true;
-      #  "editor.mouseWheelZoom" = true;
-      #  "editor.suggestSelection" = "first";
-      #  "explorer.excludeGitIgnore" = true;
-      #  "git.autofetch" = true;
-      #  "jupyter.askForKernelRestart" = false;
-      #  "jupyter.runStartupCommands" = [ "%load_ext autoreload" "%autoreload 2" ];
-      #  "redhat.telemetry.enabled" = false;
-      #  "telemetry.telemetryLevel" = "off";
-      #  "terminal.integrated.defaultProfile.linux" = "fish";
-      #  "terminal.integrated.defaultProfile.osx" = "fish";
-      #  "terminal.integrated.defaultProfile.windows" = "fish";
-      #  "terminal.integrated.enableMultiLinePasteWarning" = false;
-      #  "window.autoDetectColorScheme" = true;
-      #  "window.commandCenter" = true;
-      #  "window.restoreWindows" = "none";
-      #  "window.titleBarStyle" = "custom";
-      #  "window.zoomLevel" = 1;
-      #  "workbench.enableExperiments" = false;
-      #  "workbench.iconTheme" = "material-icon-theme";
-      #  "workbench.layoutControl.enabled" = true;
-      #  "workbench.preferredDarkColorTheme" = "Noctis Obscuro";
-      #  "workbench.preferredLightColorTheme" = "Jupyter Theme";
-      #  "workbench.settings.enableNaturalLanguageSearch" = false;
-      #};
+      extensions = with pkgs.vscode-extensions; [
+        arrterian.nix-env-selector
+        bbenoist.nix
+        davidanson.vscode-markdownlint
+        esbenp.prettier-vscode
+        github.github-vscode-theme
+        github.vscode-pull-request-github
+        gitlab.gitlab-workflow
+        hashicorp.terraform
+        jnoortheen.nix-ide
+        mkhl.direnv
+        ms-azuretools.vscode-docker
+        ms-kubernetes-tools.vscode-kubernetes-tools
+        ms-python.python
+        # TODO Unfree.
+        #ms-python.vscode-pylance
+        ms-toolsai.jupyter
+        ms-vscode.cmake-tools
+        ms-vscode.makefile-tools
+        # TODO Unfree.
+        #ms-vsliveshare.vsliveshare
+        njpwerner.autodocstring
+        pkief.material-icon-theme
+        redhat.vscode-yaml
+        stkb.rewrap
+        svelte.svelte-vscode
+        twxs.cmake
+      ];
+      userSettings = {
+        "[python]" = { "editor.defaultFormatter" = "ms-python.black-formatter"; };
+        "[yaml]" = { "editor.defaultFormatter" = "redhat.vscode-yaml"; };
+        "cloudcode.enableTelemetry" = false;
+        "cmake.configureOnOpen" = true;
+        "debug.allowBreakpointsEverywhere" = true;
+        "editor.fontFamily" = "Fantasque Sans Mono";
+        "editor.fontLigatures" = true;
+        "editor.fontSize" = 14;
+        "editor.formatOnSave" = true;
+        "editor.inlineSuggest.enabled" = true;
+        "editor.mouseWheelZoom" = true;
+        "editor.suggestSelection" = "first";
+        "explorer.excludeGitIgnore" = true;
+        "git.autofetch" = true;
+        "jupyter.askForKernelRestart" = false;
+        "jupyter.runStartupCommands" = [ "%load_ext autoreload" "%autoreload 2" ];
+        "redhat.telemetry.enabled" = false;
+        "telemetry.telemetryLevel" = "off";
+        "terminal.integrated.defaultProfile.linux" = "fish";
+        "terminal.integrated.defaultProfile.osx" = "fish";
+        "terminal.integrated.defaultProfile.windows" = "fish";
+        "terminal.integrated.enableMultiLinePasteWarning" = false;
+        "window.autoDetectColorScheme" = true;
+        "window.commandCenter" = true;
+        "window.restoreWindows" = "none";
+        "window.titleBarStyle" = "custom";
+        "window.zoomLevel" = 1;
+        "workbench.enableExperiments" = false;
+        "workbench.iconTheme" = "material-icon-theme";
+        "workbench.layoutControl.enabled" = true;
+        "workbench.preferredDarkColorTheme" = "Noctis Obscuro";
+        "workbench.preferredLightColorTheme" = "Jupyter Theme";
+        "workbench.settings.enableNaturalLanguageSearch" = false;
+      };
     };
   };
 }
