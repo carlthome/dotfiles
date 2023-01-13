bcf73e7c20da749401e23196fe1c429ec704142e
Carl Thomé <carlthome@gmail.com>
2023-01-13 16:43:44 +0100

    Add CUDA stuff to LD_LIBRARY_PATH (needed?)

--- modules/gpu.nix
+++ modules/gpu.nix
@@ -5,4 +5,14 @@
     allowUnfree = true;
     allowUnfreePredicate = pkg: true;
   };
+
+  home.packages = with pkgs; [
+    cudatoolkit
+    cudaPackages.cudnn
+    stdenv.cc.cc.lib
+  ];
+
+  home.sessionVariables = with pkgs; {
+    LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib:${cudaPackages.cudnn}/lib:${cudatoolkit}/lib:$LD_LIBRARY_PATH";
+  };
 }
