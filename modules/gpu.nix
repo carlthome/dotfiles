46f5450e21fb2094245e656c12d6d852d0174139
Carl Thomé <carlthome@gmail.com>
2023-01-15 20:29:40 +0100

    Remove stdenv.cc.cc.lib

--- modules/gpu.nix
+++ modules/gpu.nix
@@ -9,10 +9,9 @@
   home.packages = with pkgs; [
     cudatoolkit
     cudaPackages.cudnn
-    stdenv.cc.cc.lib
   ];
 
   home.sessionVariables = with pkgs; {
-    LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib:${cudaPackages.cudnn}/lib:${cudatoolkit}/lib:$LD_LIBRARY_PATH";
+    LD_LIBRARY_PATH = "${cudaPackages.cudnn}/lib:${cudatoolkit}/lib:${cudatoolkit.lib}/lib:$LD_LIBRARY_PATH";
   };
 }
