0f30c164e6031acd5642c63eb55d6a868b6dcbc4
Carl Thomé <carlthome@gmail.com>
2023-01-17 20:31:14 +0100

    Add hardware-configuration.nix and inline cachix

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -5,12 +5,21 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
-      /etc/nixos/cachix.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ../machines/t1/hardware-configuration.nix
+  ];
+
+  nix.settings.substituters = [
+    "https://cache.nixos.org/"
+    "https://cuda-maintainers.cachix.org"
+    "https://nixpkgs-unfree.cachix.org"
+  ];
+
+  nix.settings.trusted-public-keys = [
+    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+  ];
 
   # Enable Plex media server.
   services.plex.enable = true;
