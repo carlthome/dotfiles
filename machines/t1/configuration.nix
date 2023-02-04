796c1310b7812da3fc59d0d37afd517526fd1055
Carl Thomé <carlthome@gmail.com>
2023-02-04 18:20:23 +0100

    Move GPU settings into machine specifics

--- machines/t1/configuration.nix
+++ machines/t1/configuration.nix
@@ -1,3 +1,19 @@
 { config, pkgs, ... }: {
   networking.hostName = "t1";
+
+  nixpkgs.config = {
+    cudaSupport = true;
+    cudnnSupport = true;
+    allowUnfree = true;
+    allowUnfreePredicate = pkg: true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    cudatoolkit
+    cudaPackages.cudnn
+  ];
+
+  environment.sessionVariables = with pkgs; {
+    LD_LIBRARY_PATH = "${cudaPackages.cudnn}/lib:${cudatoolkit}/lib:${cudatoolkit.lib}/lib:$LD_LIBRARY_PATH";
+  };
 }
