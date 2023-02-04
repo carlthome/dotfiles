fd1869e685745d179f8e8fde4cd8535ea366ba52
Carl Thomé <carlthome@gmail.com>
2023-02-04 17:01:00 +0100

    Use flake

--- apps/switch-home/default.nix
+++ apps/switch-home/default.nix
@@ -2,7 +2,7 @@
   type = "app";
   program = (pkgs.writeScript "switch-home" ''
     set -exuo pipefail  
-    home-manager switch -b backup --flake .
+    home-manager switch -b backup --flake ${self}
     home-manager expire-generations '-30 days'
     home-manager packages
   '').outPath;
