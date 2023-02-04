cdac07435deabb92b9ad22fd2b92ebf902562e61
Carl Thomé <carlthome@gmail.com>
2023-02-04 20:12:03 +0100

    extra-trusted-public-keys

--- modules/home.nix
+++ modules/home.nix
@@ -10,7 +10,7 @@
         "https://cuda-maintainers.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
       ];
-      trusted-public-keys = [
+      extra-trusted-public-keys = [
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       ];
