a7b9852b82e13403d34f033de3263c14a0d64069
Carl Thomé <carlthome@gmail.com>
2023-03-29 22:04:19 +0200

    Set up auto-gc, sandboxing, etc.

--- modules/darwin/configuration.nix
+++ modules/darwin/configuration.nix
@@ -1,11 +1,26 @@
 { pkgs, ... }: {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
+  # Install packages in system profile.
   environment.systemPackages = [
     pkgs.vim
   ];
 
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.bash.enable = true;
+  programs.bash.enableCompletion = false;
+
+  # Recreate /run/current-system symlink after boot.
+  services.activate-system.enable = true;
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
+
+  # Enable automatic garbage collection.
+  nix.gc.automatic = true;
+
+  # Automatically deduplicate files.
+  nix.settings.auto-optimise-store = true;
+
+  # Enable sandboxing.
+  nix.settings.sandbox = true;
 }
