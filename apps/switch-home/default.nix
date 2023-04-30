e3d05ece23ac772137b0bf872007acbd824ff4f1
Carl Thomé <carlthome@gmail.com>
2023-04-30 23:27:26 +0200

    Use ${home-manager}

--- apps/switch-home/default.nix
+++ apps/switch-home/default.nix
@@ -1,9 +1,9 @@
-{ pkgs, self, ... }: {
+{ pkgs, self, home-manager, ... }: {
   type = "app";
   program = (pkgs.writeScript "switch-home" ''
-    set -exuo pipefail  
-    home-manager switch --flake ${self}
-    home-manager expire-generations '-30 days'
-    home-manager packages
+    set -exuo pipefail
+    ${home-manager}/bin/home-manager expire-generations '-30 days'
+    ${home-manager}/bin/home-manager switch --flake ${self}
+    ${home-manager}/bin/home-manager packages
   '').outPath;
 }
