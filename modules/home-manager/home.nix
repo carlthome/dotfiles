8d2c41aaf05db5dcac070e4083bfeecc02595bf5
Carl Thomé <carlthome@gmail.com>
2023-05-01 13:04:44 +0200

    Add git aliases and don't smudge LFS objects by default

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -126,7 +126,16 @@
     git = {
       enable = true;
       package = pkgs.gitFull;
-      lfs.enable = true;
+      aliases = {
+        ci = "commit";
+        co = "checkout";
+        s = "status";
+        l = "log";
+      };
+      lfs = {
+        enable = true;
+        skipSmudge = true;
+      };
       extraConfig = {
         core.editor = "vim";
         init.defaultBranch = "main";
