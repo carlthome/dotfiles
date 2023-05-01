040c18761b9ac29c9b4d77e1888e738e036a04ac
Carl Thomé <carlthome@gmail.com>
2023-05-01 15:58:14 +0200

    Configure difftool and mergetool to use vim by default (or VS Code as GUI)

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -14,8 +14,17 @@
     };
     extraConfig = {
       core.editor = "vim";
+      diff.guitool = "vscode";
+      diff.tool = "vimdiff";
+      difftool.prompt = false;
+      difftool.vscode.cmd = "code --wait --diff $LOCAL $REMOTE";
       help.autocorrect = 30;
       init.defaultBranch = "main";
+      merge.conflictstyle = "diff3";
+      merge.guitool = "vscode";
+      merge.tool = "vimdiff";
+      mergetool.prompt = false;
+      mergetool.vscode.cmd = "code --wait $MERGED";
       pull.ff = "only";
       push.autoSetupRemote = true;
       user.useConfigOnly = true;
