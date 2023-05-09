70e809507019ebd4f3b5d2709835377d83bb7760
Carl Thomé <carlthome@gmail.com>
2023-05-09 18:46:49 +0200

    Add missing whitespace

--- modules/nix-darwin/configuration.nix
+++ modules/nix-darwin/configuration.nix
@@ -19,6 +19,7 @@
 
   # Enable fingerprint scanner for authentication.
   security.pam.enableSudoTouchIdAuth = true;
+
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.bash.enable = true;
   programs.bash.enableCompletion = false;
