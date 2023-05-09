d8f07d872a2b6e76083cd0e786de52c95295b0a4
Carl Thomé <carlthome@gmail.com>
2023-05-09 18:45:18 +0200

    Rename modules/darwin to modules/nix-darwin

--- flake.nix
+++ flake.nix
@@ -4,15 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:lnl7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     home-manager = { url = "github:nix-community/home-manager/release-22.11"; inputs.nixpkgs.follows = "nixpkgs"; };
     epidemic-sound = { url = "git+ssh://git@github.com/epidemicsound/home-manager.git?ref=main"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, darwin, flake-utils, pre-commit-hooks, home-manager, epidemic-sound }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nix-darwin, flake-utils, pre-commit-hooks, home-manager, epidemic-sound }@inputs:
     let
       mapDir = d: f:
         let names = builtins.attrNames (builtins.readDir d);
@@ -27,10 +27,10 @@
         ];
       };
 
-      mkDarwin = name: darwin.lib.darwinSystem {
+      mkDarwin = name: nix-darwin.lib.darwinSystem {
         system = (import ./hosts/darwin/${name}/system.nix).system;
         modules = [
-          ./modules/darwin/configuration.nix
+          ./modules/nix-darwin/configuration.nix
           ./hosts/darwin/${name}/configuration.nix
         ];
       };
