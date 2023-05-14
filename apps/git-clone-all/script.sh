a935fb7f4dfd94a8c91591643c1b65580b10af2b
Carl Thomé <carlthome@gmail.com>
2023-05-14 15:20:17 +0200

    Add git-clone-all script

---
+++ apps/git-clone-all/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+git_root="$HOME/Repos"
+
+# List all source repos on GitHub.com for the current user.
+repos=$(gh repo list --json nameWithOwner --limit 1000 --source | jq -r '.[].nameWithOwner')
+
+# Clone all source repos that don't already exist locally.
+for repo in $repos; do
+	path="$git_root/$repo"
+	if [ -d "$path" ]; then
+		printf "Skipping %s, already exists locally\n" "$repo"
+		continue
+	fi
+	git clone git@github.com:$repo $path
+done
