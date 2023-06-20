95d2a97baa0299028b43de65cac2d4cc84b1ee33
Carl Thomé <carlthome@gmail.com>
2023-06-20 18:04:02 +0200

    Add git-stats

---
+++ apps/git-stats/script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# TODO Tabulate number of reviews conducted, landed PRs in a given time period.
+
+owner='carlthome'
+author='carlthome'
+
+# List all source repos on GitHub.com for chosen owner.
+repos=$(gh repo list "$owner" --json nameWithOwner --limit 1000 --source | jq -r '.[].nameWithOwner')
+
+# Go through all repos for PRs created by chosen author.
+for repo in $repos; do
+	prs=$(gh repo list --author="$author" $repo)
+	for pr in $prs; do
+		echo $pr
+	done
+done
