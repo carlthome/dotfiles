4682b707bf60abfbcc5d0b67eadb26644866941e
Carl Thomé <carlthome@gmail.com>
2023-06-23 12:57:47 +0200

    Allow vscode-extension-ms-vscode-remote-remote-ssh

--- modules/home-manager/x86_64-linux.nix
+++ modules/home-manager/x86_64-linux.nix
@@ -16,6 +16,7 @@
       "vscode-extension-github-copilot"
       "vscode-extension-MS-python-vscode-pylance"
       "vscode-extension-ms-vscode-cpptools"
+      "vscode-extension-ms-vscode-remote-remote-ssh"
       "vscode-extension-ms-vsliveshare-vsliveshare"
     ];
   };
