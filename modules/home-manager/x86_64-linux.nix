4e104b08eaccdd8c176901927b1c7f2c52453a14
Carl Thomé <carlthome@gmail.com>
2023-06-24 20:16:48 +0200

    Add GNOME extensions to home-manager
    As per ideas in https://hoverbear.org/blog/declarative-gnome-configuration-in-nixos/

--- modules/home-manager/x86_64-linux.nix
+++ modules/home-manager/x86_64-linux.nix
@@ -50,10 +50,35 @@
     yabridge
     yabridgectl
     zlib
-  ];
+  ] ++ (with pkgs.gnomeExtensions;
+    [
+      bing-wallpaper-changer
+      blur-my-shell
+      caffeine
+      hue-lights
+      night-theme-switcher
+      rounded-window-corners
+    ]);
 
   home.sessionVariables = with pkgs; {
     # TODO Think this through better.
     # LD_LIBRARY_PATH = "${zlib}/lib:${stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH";
+  };
+
+  # Use `dconf watch /` to track stateful changes you are doing, then set them here.
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
+        "BingWallpaper@ineffable-gmail.com"
+        "blur-my-shell@aunetx"
+        "caffeine@patapon.info"
+        "hue-lights@chlumskyvaclav.gmail.com"
+        "nightthemeswitcher@romainvigier.fr"
+        "places-menu@gnome-shell-extensions.gcampax.github.com"
+        "rounded-window-corners@yilozt"
+      ];
+    };
   };
 }
