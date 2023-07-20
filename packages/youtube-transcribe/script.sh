a94b29686fd07f0143153d4c70bee7da0c31ba39
Carl Thomé <carlthome@gmail.com>
2023-07-20 16:11:13 +0200

    Remove pipes, keep intermediate files around

--- packages/youtube-transcribe/script.sh
+++ packages/youtube-transcribe/script.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
-set -euo pipefail
+set -ex
 
-youtube_url=$1
+videoid=$1
 
-# Available models: tiny.en tiny base.en base small.en small medium.en medium large-v1 large
-model='tiny'
+# Available models: tiny.en tiny base.en base small.en small medium.en medium large-v1 large.
+model_version=${2:-'tiny'}
+model="ggml-$model_version.bin"
 
-# Download model weights (or use local cache if exists)
-whisper-cpp-download-ggml-model $model
+# Download model weights (or use local download if exists)
+whisper-cpp-download-ggml-model "$model_version"
 
-# Download YouTube video, resample audio to 16 kHz with ffmpeg, and run Whisper model on the audio stream.
-yt-dlp -o - "${youtube_url}" |
-	ffmpeg -hide_banner -loglevel error -i - -vn -acodec pcm_s16le -ar 16000 -ac 2 -f wav - |
-	whisper-cpp --print-progress --print-colors --file - --language auto --model "ggml-${model}.bin" --output-txt
+# Download audio from YouTube video.
+yt-dlp --format bestaudio --extract-audio --audio-format wav "$videoid"
+
+# Get filename of downloaded audio.
+filename=$(yt-dlp --get-filename "$videoid")
+
+# Strip file extension.
+filename="${filename%.*}"
+
+# Resample audio to 16 kHz with ffmpeg (as expected by Whisper).
+ffmpeg -loglevel warning -i "$filename.wav" -vn -acodec pcm_s16le -ar 16000 -ac 1 -f wav "$filename-16khz.wav"
+
+# Transcribe speech with Whisper model.
+whisper-cpp --file "$filename-16khz.wav" --output-file "$filename" --language auto --model "$model" --output-txt --output-srt
