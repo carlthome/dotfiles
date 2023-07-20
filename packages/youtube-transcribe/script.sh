38dfe8d8f642ecc12589d69ee8cfb9d5867ada39
Carl Thomé <carlthome@gmail.com>
2023-07-20 14:20:55 +0200

    Hide ffmpeg details

--- packages/youtube-transcribe/script.sh
+++ packages/youtube-transcribe/script.sh
@@ -11,5 +11,5 @@ whisper-cpp-download-ggml-model $model
 
 # Download YouTube video, resample audio to 16 kHz with ffmpeg, and run Whisper model on the audio stream.
 yt-dlp -o - "${youtube_url}" |
-	ffmpeg -i - -vn -acodec pcm_s16le -ar 16000 -ac 2 -f wav - |
+	ffmpeg -hide_banner -loglevel error -i - -vn -acodec pcm_s16le -ar 16000 -ac 2 -f wav - |
 	whisper-cpp --print-progress --print-colors --file - --language auto --model "ggml-${model}.bin" --output-txt
