ff90242d5bcc640f6c9905078667d636a46a8166
Carl Thomé <carlthome@gmail.com>
2023-08-20 18:39:30 +0200

    Fix SC2086

--- packages/git-stats/script.sh
+++ packages/git-stats/script.sh
@@ -5,10 +5,10 @@ set -e
 year=$(date +%Y)
 
 # Extract pull request information.
-num_reviewed=$(gh search prs --reviewed-by=@me --created=$year --limit=1000 --json title --jq '.[].title' | wc --lines)
-num_created=$(gh search prs --author=@me --created=$year --limit=1000 --json title --jq '.[].title' | wc --lines)
-num_landed=$(gh search prs --author=@me --merged --created=$year --limit=1000 --json title --jq '.[].title' | wc --lines)
-num_involved=$(gh search prs --involves=@me --created=$year --limit=1000 --json title --jq '.[].title' | wc --lines)
+num_reviewed=$(gh search prs --reviewed-by=@me --created="$year" --limit=1000 --json title --jq '.[].title' | wc --lines)
+num_created=$(gh search prs --author=@me --created="$year" --limit=1000 --json title --jq '.[].title' | wc --lines)
+num_landed=$(gh search prs --author=@me --merged --created="$year" --limit=1000 --json title --jq '.[].title' | wc --lines)
+num_involved=$(gh search prs --involves=@me --created="$year" --limit=1000 --json title --jq '.[].title' | wc --lines)
 
 # Print results.
 echo "Number of PRs reviewed: $num_reviewed"
