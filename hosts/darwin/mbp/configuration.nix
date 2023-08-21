057909407e6566b49df0d3017a106f3df68a19c2
Carl Thomé <carlthome@gmail.com>
2023-08-21 12:51:53 +0200

    Set kubernetes.default.svc.cluster.local to 127.0.0.1

--- hosts/darwin/mbp/configuration.nix
+++ hosts/darwin/mbp/configuration.nix
@@ -1,3 +1,12 @@
 { pkgs, ... }: {
   networking.hostName = "mbp";
+  environment.etc.hosts = {
+    copy = true;
+    text = ''
+      127.0.0.1	      localhost
+      255.255.255.255 broadcasthost
+      ::1             localhost
+      127.0.0.1       kubernetes.default.svc.cluster.local
+    '';
+  };
 }
