75f9ccfa132132a509bb8d2cb966c1a362a34a53
Carl Thomé <carlthome@gmail.com>
2023-10-16 21:53:46 +0200

    Get VS Code extensions from nixpkgs-unstable

--- homes/default.nix
+++ homes/default.nix
@@ -7,11 +7,17 @@ let
           inherit super system;
           config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
             "vscode"
+            "vscode-extension-github-copilot"
+            "vscode-extension-MS-python-vscode-pylance"
+            "vscode-extension-ms-vscode-cpptools"
+            "vscode-extension-ms-vscode-remote-remote-ssh"
+            "vscode-extension-ms-vsliveshare-vsliveshare"
           ];
         };
       in
       {
         vscode = pkgs.vscode;
+        vscode-extensions = pkgs.vscode-extensions;
       }
     )
   ];
