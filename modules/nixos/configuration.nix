5f1a73075c946fe0adfdc38aa441de9d0e8e947e
Carl Thomé <carlthome@gmail.com>
2023-11-27 18:25:35 +0100

    Remove dead code

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -108,12 +108,6 @@
     flake = "nixpkgs";
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
