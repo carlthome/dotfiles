0716186c1456014a493f73fccd9588043d662a4a
Carl Thomé <carlthome@gmail.com>
2023-11-28 22:52:01 +0100

    Use `tee` and set shebang

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+set -x
+
 # Install Nix.
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
 # Enable flakes and the new command-line interface.
-echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
+echo "experimental-features = nix-command flakes" | tee ~/.config/nix/nix.conf
