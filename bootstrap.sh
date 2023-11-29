1c56c1909a10635947c0f0fe0ab483d5a3565c16
Carl Thomé <carlthome@gmail.com>
2023-11-29 13:20:32 +0100

    Create ~/.config/nix directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,5 @@ set -x
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
 # Enable flakes and the new command-line interface.
+mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" | tee ~/.config/nix/nix.conf
