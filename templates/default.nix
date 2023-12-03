89e1a6529df1a6616e661b2b323db04c8a4597aa
Carl Thomé <carlthome@gmail.com>
2023-12-04 00:25:28 +0100

    Fix checksums

--- templates/default.nix
+++ templates/default.nix
@@ -2,21 +2,21 @@
   nix-jax-mnist = {
     path = (builtins.fetchTarball {
       url = "https://github.com/carlthome/nix-jax-mnist/archive/refs/tags/v1.0.0.zip";
-      sha256 = "1dwrhj9vj03x6ni9jgyb6fvbm48ply392sjh5d1x4a2hn4h5ydjx";
+      sha256 = "16pn51kc39c6ifvabr0mpgpvg4qnpr4j303g66jbr61ifk7ybyz5";
     });
     description = "A fully-functional example of using JAX to train a MNIST classifier with Nix";
   };
   poetry2nix-example = {
     path = (builtins.fetchTarball {
       url = "https://github.com/carlthome/poetry2nix-example/archive/refs/tags/v1.0.0.zip";
-      sha256 = "1z86j01k5fji883fl6n1fmp49z9ckrgzrqz6v75g8wnb3n81gw7l";
+      sha256 = "04fwkyddj1pc2nhsg49fc1xh4irjgwy2plcp9b1rbxa0qr5bmjz9";
     });
     description = "A fully-functional example of using Poetry with Nix";
   };
   nix-pip-flake = {
     path = (builtins.fetchTarball {
       url = "https://github.com/carlthome/nix-pip-flake/archive/refs/tags/v1.0.0.zip";
-      sha256 = "02dk1zg2dag3c3d6hh5yjd0bip10sl5ifdz07cvw85cq8i5gmfly";
+      sha256 = "1bswhb705kxpcr82a6yfifjwawifyj1ycdy1w4a471rzf3xkk03v";
     });
     description = "A fully-functional example of using a Python virtual environment with Nix";
   };
