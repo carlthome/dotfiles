25fd71c864f15e67d0104bb57333063d8f960798
Carl Thomé <carlthome@gmail.com>
2023-12-16 12:58:44 +0100

    Enable enableSSHAgentAuth

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -2,6 +2,7 @@
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "no";
   services.openssh.settings.PasswordAuthentication = false;
+  security.pam.enableSSHAgentAuth = true;
   users.mutableUsers = false;
   documentation.man.enable = false;
 }
