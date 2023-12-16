2c266f753e71f76a25a47aa0fc5a07b91e7ddf61
Carl Thomé <carlthome@gmail.com>
2023-12-16 14:52:15 +0100

    Enable sudo.sshAgentAuth

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -3,6 +3,7 @@
   services.openssh.settings.PermitRootLogin = "no";
   services.openssh.settings.PasswordAuthentication = false;
   security.pam.enableSSHAgentAuth = true;
+  security.pam.services.sudo.sshAgentAuth = true;
   users.mutableUsers = false;
   documentation.man.enable = false;
 }
