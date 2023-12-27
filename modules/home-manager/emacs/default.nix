a0081c54d691f284c11b3979c030823aaf452be3
Carl Thomé <carlthome@gmail.com>
2023-12-27 15:28:01 +0100

    Add emms and magit

--- modules/home-manager/emacs/default.nix
+++ modules/home-manager/emacs/default.nix
@@ -1,5 +1,9 @@
 { config, pkgs, lib, ... }: {
-  programs = {
-    emacs.enable = true;
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: with epkgs; [
+      emms
+      magit
+    ];
   };
 }
