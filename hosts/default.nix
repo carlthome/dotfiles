3e44b3034203ffbf2957e054a41f7c8d342188ba
Carl Thomé <carlthome@gmail.com>
2023-12-31 16:07:09 +0100

    Inline default configuration and move home configuration to modules

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,8 +1,37 @@
-{ nixpkgs, nix-darwin, self, ... }: {
+{ nixpkgs, nix-darwin, self, ... }:
+let
+  configuration = {
+    # Configure the `nix` program itself.
+    nix.settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://carlthome.cachix.org"
+        "https://numtide.cachix.org"
+      ];
+      trusted-public-keys = [
+        "carlthome.cachix.org-1:BHerYg0J5Qv/Yw/SsxqPBlTY+cttA9axEsmrK24R15w="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      ];
+      auto-optimise-store = true;
+      cores = 1;
+      max-jobs = 1;
+    };
+
+    # Link old commands (nix-shell, nix-build, etc.) to use the same nixpkgs as the flake.
+    nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+
+    # Enable automatic garbage collection.
+    nix.gc = {
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
+  };
+in
+{
   t1 = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
-      ../configuration.nix
+      configuration
       ./t1/configuration.nix
       ./t1/hardware-configuration.nix
       self.nixosModules.default
@@ -14,7 +43,7 @@
   pi = nixpkgs.lib.nixosSystem {
     system = "aarch64-linux";
     modules = [
-      ../configuration.nix
+      configuration
       "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
       { sdImage.compressImage = false; }
       { nixpkgs.overlays = [ self.overlays.modules-closure ]; }
@@ -28,7 +57,7 @@
   mbp = nix-darwin.lib.darwinSystem {
     system = "aarch64-darwin";
     modules = [
-      ../configuration.nix
+      configuration
       ./mbp/configuration.nix
       self.darwinModules.default
     ];
@@ -37,7 +66,7 @@
   Betty = nix-darwin.lib.darwinSystem {
     system = "aarch64-darwin";
     modules = [
-      ../configuration.nix
+      configuration
       ./Betty/configuration.nix
       self.darwinModules.default
     ];
