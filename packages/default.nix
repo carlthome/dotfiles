e73851377632cd8c900d934c35d6ce3e0c34942d
Carl Thomé <carlthome@gmail.com>
2024-01-03 14:59:08 +0100

    Use unstable by default

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,6 @@
-{ nixpkgs, system, ... }@inputs:
+{ nixpkgs-unstable, system, ... }@inputs:
 let
-  pkgs = nixpkgs.legacyPackages.${system};
+  pkgs = nixpkgs-unstable.legacyPackages.${system};
   names = builtins.attrNames (pkgs.lib.filterAttrs (n: v: v == "directory") (builtins.readDir ./.));
   mkPackage = name: pkgs.callPackage ./${name} inputs;
   packages = pkgs.lib.genAttrs names mkPackage;
