1e93a6922ff3178259e58bbe88d1528a6c39eec3
Carl Thomé <carlthome@gmail.com>
2024-03-12 23:10:11 +0100

    Remove extra pane split shortcuts

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -26,11 +26,6 @@
       set -ga terminal-overrides ",*256col*:Tc"
       set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
       set-environment -g COLORTERM "truecolor"
-
-      # Memorable pane split shortcuts.
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
-      bind c new-window -c "#{pane_current_path}"
     '';
   };
 }
