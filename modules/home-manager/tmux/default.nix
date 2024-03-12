0161f8bbc58dfa05173e73df7b36fa764edb2836
Carl Thomé <carlthome@gmail.com>
2024-03-12 23:09:43 +0100

    Enable automatic session restore

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -15,6 +15,9 @@
       # Enable mouse mode.
       set-option -g mouse on
 
+      # Automatic session restore.
+      set -g @continuum-restore 'on'
+
       set -g status-right '#[fg=black,bg=color15] #{cpu_percentage}  %H:%M '
       run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
 
