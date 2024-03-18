36ef20ee1c80b560dc0558358144c635d8855103
Carl Thomé <carlthome@gmail.com>
2024-03-18 23:14:24 +0100

    Restore default look

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -3,9 +3,7 @@
     enable = true;
     plugins = with pkgs.tmuxPlugins; [
       better-mouse-mode
-      cpu
       sensible
-      sidebar
       resurrect
       continuum
     ];
@@ -15,16 +13,6 @@
 
       # Automatic session restore.
       set -g @continuum-restore 'on'
-
-      # Display CPU usage in the status bar.
-      set -g status-right '#[fg=black,bg=color15] #{cpu_percentage}  %H:%M '
-      run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
-
-      # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-      set -g default-terminal "xterm-256color"
-      set -ga terminal-overrides ",*256col*:Tc"
-      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
-      set-environment -g COLORTERM "truecolor"
     '';
   };
 }
