a154c2674f6cf39ac3ef20a82eb82ddf6ad76394
Carl Thomé <carlthome@gmail.com>
2024-04-11 16:50:15 +0200

    Garbage collect more

--- hosts/Betty/configuration.nix
+++ hosts/Betty/configuration.nix
@@ -1,3 +1,16 @@
 { pkgs, ... }: {
   networking.hostName = "Betty";
+
+  # Delete old store paths since the disk is only 256 GB.
+  nix.gc = {
+    automatic = true;
+    interval = { Weekday = 0; Hour = 0; Minute = 0; };
+    options = "--delete-older-than 30d";
+  };
+
+  # Automatically delete store paths when running out of disk space.
+  nix.extraOptions = ''
+    min-free = ${toString (100 * 1024 * 1024)}
+    max-free = ${toString (1024 * 1024 * 1024)}
+  '';
 }
