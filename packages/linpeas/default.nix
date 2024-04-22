e9a9e8fe3cbb9eb9ea3cdcf0f5af80e2e9b7c2b3
Carl Thomé <carlthome@gmail.com>
2024-04-22 23:41:19 +0200

    Add linpeas

---
+++ packages/linpeas/default.nix
@@ -0,0 +1,10 @@
+{ lib
+, writeScriptBin
+, fetchurl
+, ...
+}:
+
+writeScriptBin "linpeas" (builtins.readFile (fetchurl {
+  url = "https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh";
+  hash = "sha256-6DcTBxZk2Lb+65yULwXco7pJUaxVdD2663mTP0coln0=";
+}))
