14528e1cbfa7b78162de49ad1941539fad217024
Carl Thomé <carlthome@gmail.com>
2024-04-23 00:01:32 +0200

    Open any text in $EDITOR

--- packages/fzf-open/script.sh
+++ packages/fzf-open/script.sh
@@ -5,14 +5,17 @@ query=${1:-""}
 match=$(fd --color=always | fzf --query="$query" --no-multi --ansi)
 echo "Selected: $match"
 
+# Get the MIME type of the selected path.
+mimetype=$(file --mime-type -b "$match")
+echo "MIME type: $mimetype"
+
 # Open the selected path.
 if [ -z "$match" ]; then
 	exit 0
 elif [ -d "$match" ]; then
 	echo "Opening $match in $SHELL"
-	cd "$match" || exit 1
-	exec $SHELL
-elif file --mime-type -b "$match" | grep -q 'text/plain'; then
+	cd "$match" && exec $SHELL
+elif echo "$mimetype" | grep -q 'text/'; then
 	echo "Opening $match in $EDITOR"
 	$EDITOR "$match"
 elif [ -f "$match" ]; then
