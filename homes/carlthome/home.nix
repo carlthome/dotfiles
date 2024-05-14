eacc8385150b6dcf7397555f02697758c68577a2
Carl Thomé <carlthome@gmail.com>
2024-05-14 23:05:34 +0200

    Add miniconda

--- homes/carlthome/home.nix
+++ homes/carlthome/home.nix
@@ -16,4 +16,19 @@
   home.homeDirectory = if pkgs.stdenv.hostPlatform.isDarwin then "/Users/carlthome" else "/home/carlthome";
   programs.git.userName = "Carl Thomé";
   programs.git.userEmail = "carl.thome@epidemicsound.com";
+  home.sessionPath = [
+    "$HOME/miniconda3/bin"
+  ];
+  home.file.".condarc" = {
+    text = ''
+      channels:
+        - conda-forge
+        - defaults
+        - pytorch
+      channel_priority: strict
+      auto_activate_base: false
+      default_threads: 4
+    '';
+    executable = false;
+  };
 }
