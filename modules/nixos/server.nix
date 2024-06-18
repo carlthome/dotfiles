5970d73160f22cdb2503ce8aeb7423cdb1049b25
Carl Thomé <carlthome@gmail.com>
2024-06-18 20:11:12 +0200

    Fix deprecated setting

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -2,7 +2,7 @@
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "no";
   services.openssh.settings.PasswordAuthentication = false;
-  security.pam.enableSSHAgentAuth = true;
+  security.pam.sshAgentAuth.enable = true;
   security.pam.services.sudo.sshAgentAuth = true;
   users.mutableUsers = false;
   documentation.man.enable = false;
