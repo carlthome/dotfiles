1a95b4c6be80b0dbbca30467c9e9f8d4b5cc2e09
Carl Thomé <carlthome@gmail.com>
2024-07-17 00:26:27 +0200

    Don't allow unfree

--- packages/default.nix
+++ packages/default.nix
@@ -1,25 +1,6 @@
 { nixpkgs-unstable, mirpkgs, system, ... }@inputs:
 let
-  pkgs = import nixpkgs-unstable {
-    inherit system; overlays = [ mirpkgs.overlays.default ];
-    config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs-unstable.lib.getName pkg) [
-      "cuda_cccl"
-      "cuda_cudart"
-      "cuda_cupti"
-      "cuda_nvcc"
-      "cuda_nvrtc"
-      "cuda_nvtx"
-      "cudnn"
-      "libcublas"
-      "libcufft"
-      "libcurand"
-      "libcusolver"
-      "libcusparse"
-      "libnvjitlink"
-      "torch"
-      "triton"
-    ];
-  };
+  pkgs = import nixpkgs-unstable { inherit system; overlays = [ mirpkgs.overlays.default ]; };
   names = builtins.attrNames (pkgs.lib.filterAttrs (n: v: v == "directory") (builtins.readDir ./.));
   mkPackage = name: pkgs.callPackage ./${name} inputs;
   packages = pkgs.lib.genAttrs names mkPackage;
