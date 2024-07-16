0f541d06c327e1b92b168ddf1e183c9b84486f92
Carl Thomé <carlthome@gmail.com>
2024-07-16 23:55:08 +0200

    Allow CUDA

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,25 @@
 { nixpkgs-unstable, mirpkgs, system, ... }@inputs:
 let
-  pkgs = import nixpkgs-unstable { inherit system; overlays = [ mirpkgs.overlays.default ]; };
+  pkgs = import nixpkgs-unstable {
+    inherit system; overlays = [ mirpkgs.overlays.default ];
+    config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs-unstable.lib.getName pkg) [
+      "cuda_cccl"
+      "cuda_cudart"
+      "cuda_cupti"
+      "cuda_nvcc"
+      "cuda_nvrtc"
+      "cuda_nvtx"
+      "cudnn"
+      "libcublas"
+      "libcufft"
+      "libcurand"
+      "libcusolver"
+      "libcusparse"
+      "libnvjitlink"
+      "torch"
+      "triton"
+    ];
+  };
   names = builtins.attrNames (pkgs.lib.filterAttrs (n: v: v == "directory") (builtins.readDir ./.));
   mkPackage = name: pkgs.callPackage ./${name} inputs;
   packages = pkgs.lib.genAttrs names mkPackage;
