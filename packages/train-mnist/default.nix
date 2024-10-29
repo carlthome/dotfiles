4aa5ebffd85ecf815aac6c13d9d9a2e7eb1f7ec1
Carl Thomé <carlthome@gmail.com>
2024-10-29 12:23:55 +0100

    Use latest Python 3 by default

--- packages/train-mnist/default.nix
+++ packages/train-mnist/default.nix
@@ -1,12 +1,12 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
-pkgs.python311Packages.buildPythonApplication {
+pkgs.python3Packages.buildPythonApplication {
   pname = "mnist";
   version = "0.1.0";
   src = ./.;
   format = "pyproject";
 
-  nativeBuildInputs = with pkgs.python311Packages; [
+  nativeBuildInputs = with pkgs.python3Packages; [
     setuptools
     wheel
     pytestCheckHook
@@ -14,7 +14,7 @@ pkgs.python311Packages.buildPythonApplication {
 
   importChecks = [ "mnist" ];
 
-  propagatedBuildInputs = with pkgs.python311Packages; [
+  propagatedBuildInputs = with pkgs.python3Packages; [
     fire
     torch
     torchaudio
