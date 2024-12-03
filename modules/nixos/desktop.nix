78b92b34acf6fb8e2fb7a56ed0fc98b997aaffab
Carl Thomé <carlthome@gmail.com>
2024-12-03 22:33:56 +0100

    hardware.opengl -> hardware.graphics

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
 
   # Configure graphics settings.
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
