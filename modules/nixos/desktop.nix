5ca0e861eeb8bc30b35ffcd9d250f4ff69a77863
Carl Thomé <carlthome@gmail.com>
2024-12-08 15:13:52 +0100

    Configure realtime audio settings for audio group members

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -20,6 +20,13 @@
     jack.enable = false;
   };
 
+  security.pam.loginLimits = [
+    { domain = "@audio"; item = "memlock"; type = "-"; value = "unlimited"; }
+    { domain = "@audio"; item = "rtprio"; type = "-"; value = "99"; }
+    { domain = "@audio"; item = "nofile"; type = "soft"; value = "99999"; }
+    { domain = "@audio"; item = "nofile"; type = "hard"; value = "99999"; }
+  ];
+
   # Enable networking.
   networking.networkmanager.enable = true;
 
