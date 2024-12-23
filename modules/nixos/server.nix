ecf4d1b0eb2ab92d13799c2af49a1756b7707a52
Carl Thomé <carlthome@gmail.com>
2024-12-23 18:39:07 +0100

    Remove additional default software

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -1,9 +1,34 @@
 { config, pkgs, ... }: {
+
+  # Configure key-based remote SSH access.
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "no";
   services.openssh.settings.PasswordAuthentication = false;
   security.pam.sshAgentAuth.enable = true;
   security.pam.services.sudo.sshAgentAuth = true;
+
+  # Disable sudo by default.
+  security.sudo.enable = false;
+
+  # Insist all users are declaratively defined.
   users.mutableUsers = false;
+
+  # Clear out the default user environment.
+  environment.defaultPackages = [ ];
+
+  # Skip installing documentation on the server.
+  documentation.enable = false;
+  documentation.doc.enable = false;
+  documentation.info.enable = false;
   documentation.man.enable = false;
+  documentation.nixos.enable = false;
+
+  # Skip the program that suggests installable software.
+  programs.command-not-found.enable = false;
+
+  # Disable desktop-specific functionality.
+  xdg.autostart.enable = false;
+  xdg.icons.enable = false;
+  xdg.mime.enable = false;
+  xdg.sounds.enable = false;
 }
