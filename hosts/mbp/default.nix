be3f4ba99dd568fa4f01dec2b488bee9e939934b
Carl Thomé <carlthome@gmail.com>
2025-01-03 00:09:02 +0100

    Remove redundant workaround since nix-darwin use is assumed

--- hosts/mbp/default.nix
+++ hosts/mbp/default.nix
@@ -4,7 +4,6 @@ nix-darwin.lib.darwinSystem {
   system = "aarch64-darwin";
   modules = [
     ./configuration.nix
-    ({ lib, ... }: { nix.settings.auto-optimise-store = lib.mkForce false; }) # TODO https://github.com/NixOS/nix/issues/7273
     self.darwinModules.auto-upgrade
     self.darwinModules.default
     self.modules.default
