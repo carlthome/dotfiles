1feb3ff02b23f818a3792aeddfb207d8339d3748
Carl Thomé <carlthome@gmail.com>
2025-01-02 23:09:56 +0100

    Listen to localhost only

--- hosts/pi/configuration.nix
+++ hosts/pi/configuration.nix
@@ -201,7 +201,7 @@ in
     enable = true;
     settings = {
       server = {
-        http_addr = "0.0.0.0";
+        http_addr = "127.0.0.1";
         http_port = 3000;
         domain = "localhost";
       };
