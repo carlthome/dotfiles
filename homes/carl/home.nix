069cc5935c921736e3efc1196afff7dc82f2858c
Carl Thomé <carlthome@gmail.com>
2025-01-02 23:55:47 +0100

    Expose flake URI as module option

--- homes/carl/home.nix
+++ homes/carl/home.nix
@@ -3,4 +3,8 @@
   home.homeDirectory = if pkgs.stdenv.hostPlatform.isDarwin then "/Users/carl" else "/home/carl";
   programs.git.userName = "Carl Thomé";
   programs.git.userEmail = "carlthome@gmail.com";
+  services.auto-upgrade = {
+    enable = true;
+    flake = "github:carlthome/dotfiles";
+  };
 }
--- modules/home-manager/auto-upgrade/default.nix
+++ modules/home-manager/auto-upgrade/default.nix
@@ -1,37 +1,37 @@
-{ pkgs, ... }:
-let
-  program = ((pkgs.writeShellApplication
-    {
-      name = "auto-upgrade";
-      runtimeInputs = with pkgs; [ nix home-manager ];
-      text = "home-manager switch --refresh --flake github:carlthome/dotfiles";
-    }
-  ).outPath + "/bin/auto-upgrade");
-in
-{
-  launchd.agents.auto-upgrade = {
-    enable = true;
-    config = {
-      ProgramArguments = [ program ];
-      ProcessType = "Background";
-      StartCalendarInterval = [{ Hour = 0; Minute = 0; }];
-      StandardErrorPath = "/tmp/auto-upgrade-home.err";
-      StandardOutPath = "/tmp/auto-upgrade-home.out";
+{ config, pkgs, lib, ... }: {
+  options.services.auto-upgrade = {
+    enable = lib.mkEnableOption "Automatic home-manager upgrades";
+    flake = lib.mkOption {
+      type = lib.types.str;
+      description = "Flake URI to use for upgrades";
     };
   };
-  systemd.user = {
-    timers.home-manager-auto-upgrade = {
-      Unit.Description = "Home Manager upgrade timer";
-      Install.WantedBy = [ "timers.target" ];
-      Timer = {
-        OnCalendar = "daily";
-        Unit = "home-manager-auto-upgrade.service";
-        Persistent = true;
+
+  config = lib.mkIf config.services.auto-upgrade.enable {
+    launchd.agents.auto-upgrade = {
+      enable = true;
+      config = {
+        ProgramArguments = [ "${pkgs.home-manager}/bin/home-manager" "switch" "--refresh" "--flake" config.services.auto-upgrade.flake ];
+        ProcessType = "Background";
+        StartCalendarInterval = [{ Hour = 0; Minute = 0; }];
+        StandardErrorPath = "/tmp/auto-upgrade-home.err";
+        StandardOutPath = "/tmp/auto-upgrade-home.out";
       };
     };
-    services.home-manager-auto-upgrade = {
-      Unit.Description = "Home Manager upgrade";
-      Service.ExecStart = program;
+    systemd.user = {
+      timers.home-manager-auto-upgrade = {
+        Unit.Description = "Home Manager upgrade timer";
+        Install.WantedBy = [ "timers.target" ];
+        Timer = {
+          OnCalendar = "daily";
+          Unit = "home-manager-auto-upgrade.service";
+          Persistent = true;
+        };
+      };
+      services.home-manager-auto-upgrade = {
+        Unit.Description = "Home Manager upgrade";
+        Service.ExecStart = "${pkgs.home-manager}/bin/home-manager switch --refresh --flake ${config.services.auto-upgrade.flake}";
+      };
     };
   };
 }
