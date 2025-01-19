ab13ea0fd3ac1d5a1f449866df656f464abc4916
Carl Thomé <carlthome@gmail.com>
2025-01-20 00:36:06 +0100

    Configure default gateway

--- systems/pi/configuration.nix
+++ systems/pi/configuration.nix
@@ -77,17 +77,23 @@ in
 
   networking = {
     hostName = "pi";
-    useDHCP = false;
-    interfaces."wlan0" = {
-      ipv4.addresses = [{
-        address = "192.168.0.2";
-        prefixLength = 24;
-      }];
-    };
+
     wireless = {
       enable = true;
       interfaces = [ "wlan0" ];
     };
+
+    defaultGateway = {
+      address = "192.168.0.1";
+      interface = "wlan0";
+    };
+
+    interfaces."wlan0" = {
+      ipv4.addresses = [
+        { address = "192.168.0.2"; prefixLength = 24; }
+      ];
+    };
+
     firewall = {
       allowedTCPPorts = [
         80 # HTTP
