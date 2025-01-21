e663a44f6d0dd6b587f68ccc9cb65288cdc1291b
Carl Thomé <carlthome@gmail.com>
2025-01-21 22:36:02 +0100

    Use `route` on macOS

--- packages/display-lan/script.sh
+++ packages/display-lan/script.sh
@@ -4,10 +4,17 @@ set -x
 # List open ports on the current system.
 lsof -Pn -i4 | grep LISTEN
 
+# Look up the default network interface.
+if [ "$(uname)" = "Darwin" ]; then
+	interface=$(route -n get default | grep interface | awk '{print $2}')
+	ip_range=$(ifconfig "$interface" | grep 'inet ' | awk '{print $2"/24"}')
+else
+	interface=$(ip route | grep default | awk '{print $5}')
+	ip_range=$(ip -o -f inet addr show "$interface" | awk '{print $4}')
+fi
+
 # Find devices on the network with arp-scan.
-interface=$(ip route | grep default | awk '{print $5}')
 sudo arp-scan -l -I "$interface"
 
 # Scan the network for open ports with nmap.
-ip_range=$(ip -o -f inet addr show "$interface" | awk '{print $4}')
 nmap -T5 "$ip_range"
