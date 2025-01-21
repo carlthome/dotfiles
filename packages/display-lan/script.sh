8b33418ffdf193e6eae8d54c99dbaedb71893558
Carl Thomé <carlthome@gmail.com>
2025-01-21 22:29:47 +0100

    Look up default network interface and IP range

--- packages/display-lan/script.sh
+++ packages/display-lan/script.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -x
 
+# List open ports on the current system.
 lsof -Pn -i4 | grep LISTEN
 
-sudo arp-scan -l -I en0
+# Find devices on the network with arp-scan.
+interface=$(ip route | grep default | awk '{print $5}')
+sudo arp-scan -l -I "$interface"
 
-nmap -T5 '192.168.0.*'
+# Scan the network for open ports with nmap.
+ip_range=$(ip -o -f inet addr show "$interface" | awk '{print $4}')
+nmap -T5 "$ip_range"
