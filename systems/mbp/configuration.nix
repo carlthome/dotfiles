c9845358a6af3b7948b66a37edbff670a9987ac3
Carl Thomé <carlthome@gmail.com>
2025-02-03 17:23:40 +0100

    Remove /etc/hosts

--- systems/mbp/configuration.nix
+++ systems/mbp/configuration.nix
@@ -1,14 +1,6 @@
 { pkgs, ... }:
 {
   networking.hostName = "mbp";
-  environment.etc.hosts = {
-    text = ''
-      127.0.0.1	      localhost
-      255.255.255.255 broadcasthost
-      ::1             localhost
-      127.0.0.1       kubernetes.default.svc.cluster.local
-    '';
-  };
 
   services.auto-upgrade = {
     enable = true;
