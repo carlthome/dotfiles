85e8ddc4d72a1ca8e36638497df3f353c0bc9025
Carl Thomé <carlthome@gmail.com>
2025-02-22 18:28:53 +0100

    Remove kubernetes localhost config

--- systems/t1/configuration.nix
+++ systems/t1/configuration.nix
@@ -37,10 +37,6 @@
     };
   };
 
-  networking.extraHosts = ''
-    127.0.0.1 kubernetes.default.svc.cluster.local
-  '';
-
   services.restic.backups = {
     datasets = {
       repository = "rclone:gdrive:/Datasets";
