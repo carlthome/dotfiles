549334c9758b65449b687ae9d2512297e4fb86aa
Carl Thomé <carlthome@gmail.com>
2025-04-21 15:46:11 +0200

    Tweak gc settings

--- systems/mba/configuration.nix
+++ systems/mba/configuration.nix
@@ -2,21 +2,10 @@
 {
   networking.hostName = "mba";
 
-  # Delete old store paths since the disk is only 256 GB.
-  nix.gc = {
-    automatic = true;
-    interval = {
-      Weekday = 0;
-      Hour = 0;
-      Minute = 0;
-    };
-    options = "--delete-older-than 30d";
-  };
-
-  # Automatically delete store paths when running out of disk space.
+  # Since the disk is only 256 GB, garbage collect store paths when running out of space.
   nix.extraOptions = ''
-    min-free = ${toString (100 * 1024 * 1024)}
-    max-free = ${toString (1024 * 1024 * 1024)}
+    min-free = 100M
+    max-free = 1G
   '';
 
   services.wallpaper = {
