be4b972bcd227bae1896210fc254ceda12960f66
Carl Thomé <carlthome@gmail.com>
2025-06-01 22:24:32 +0200

    Stop relying on switch-home and switch-system packages
    And always check the flake and build the default package first.

--- packages/update-and-switch/script.sh
+++ packages/update-and-switch/script.sh
@@ -7,9 +7,25 @@ git pull
 # Update flake lock file.
 nix flake update --commit-lock-file --flake .
 
-# Switch system and home configuration.
-nix run .#switch-system
-nix run .#switch-home
+# Check evaluation before switching.
+nix flake check --all-systems
+
+# Build default package.
+nix build
+
+# Switch home configuration.
+home-manager switch --flake .
+
+# Switch system configuration.
+OS_TYPE="$(uname)"
+if [ "$OS_TYPE" = "Darwin" ]; then
+	sudo darwin-rebuild switch --flake .
+elif [ "$OS_TYPE" = "Linux" ]; then
+	sudo nixos-rebuild switch --flake .
+else
+	echo "Unknown OS: $OS_TYPE"
+	exit 1
+fi
 
 # Update remote repository.
 git push
