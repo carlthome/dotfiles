aca7d50818b0c4ea6baf1b05518b9c2b0d2814e2
Carl Thomé <carlthome@gmail.com>
2025-06-11 16:13:38 +0200

    Suffix name

--- modules/home-manager/auto-upgrade/default.nix
+++ modules/home-manager/auto-upgrade/default.nix
@@ -10,7 +10,7 @@ let
   cfg = config.services.auto-upgrade;
 
   upgradeScript = pkgs.writeShellApplication {
-    name = "auto-upgrade";
+    name = "auto-upgrade-home-manager";
     text = ''
       echo "Starting Home Manager upgrade at $(date)"
       home-manager switch --refresh --flake ${cfg.flake}
