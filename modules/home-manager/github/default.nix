0d74bd4700266095535a9d8a417538cbcdcc4842
Carl Thomé <carlthome@gmail.com>
2025-07-10 23:13:05 +0200

    Register `gh dashboard`

--- modules/home-manager/github/default.nix
+++ modules/home-manager/github/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, self, ... }:
+{
+  pkgs,
+  lib,
+  self,
+  ...
+}:
 {
   programs.gh = {
     enable = true;
@@ -8,6 +13,9 @@
       aliases = {
         clone-all = "!${self.packages.${pkgs.system}.github-clone-all}/bin/github-clone-all";
         stats = "!${self.packages.${pkgs.system}.github-stats}/bin/github-stats";
+        dashboard = "!${
+          self.packages.${pkgs.system}.github-actions-dashboard-creator
+        }/bin/github-actions-dashboard-creator";
       };
     };
     gitCredentialHelper.enable = false;
