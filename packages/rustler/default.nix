290d8d5918af1da2edd5d578495eea7f1ceb8922
Carl Thomé <carlthome@gmail.com>
2025-07-13 22:35:17 +0200

    Look for linux instead

--- packages/rustler/default.nix
+++ packages/rustler/default.nix
@@ -11,31 +11,33 @@ pkgs.rustPlatform.buildRustPackage {
     lockFile = ./Cargo.lock;
   };
   nativeBuildInputs = [ pkgs.pkg-config ];
-  buildInputs = with pkgs; lib.optionals (!stdenv.isDarwin) [
-    glib
-    gtk3
-    cairo
-    pango
-    gdk-pixbuf
-    glibc
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXi
-    xorg.libXext
-    xorg.libXinerama
-    xorg.libXxf86vm
-    xorg.libXrender
-    xorg.libxcb
-    xorg.libXau
-    xorg.libXdmcp
-    mesa
-    alsa-lib
-    dbus
-    freetype
-    fontconfig
-    zlib
-  ];
+  buildInputs =
+    with pkgs;
+    pkgs.lib.optionals pkgs.stdenv.isLinux [
+      glib
+      gtk3
+      cairo
+      pango
+      gdk-pixbuf
+      glibc
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      xorg.libXext
+      xorg.libXinerama
+      xorg.libXxf86vm
+      xorg.libXrender
+      xorg.libxcb
+      xorg.libXau
+      xorg.libXdmcp
+      mesa
+      alsa-lib
+      dbus
+      freetype
+      fontconfig
+      zlib
+    ];
   postInstall = ''
     cp -r resources $out/bin
   '';
