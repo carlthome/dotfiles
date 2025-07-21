f292dd5472120687656124a0a3539daac1f41c94
Carl Thomé <carlthome@gmail.com>
2025-07-21 17:27:52 +0200

    Move game source to another repo

--- packages/rustler/default.nix
+++ packages/rustler/default.nix
@@ -3,43 +3,11 @@
   ...
 }:
 
-pkgs.rustPlatform.buildRustPackage {
-  pname = "rustler";
-  version = "0.1.0";
-  src = ./.;
-  cargoLock = {
-    lockFile = ./Cargo.lock;
+let
+  src = fetchGit {
+    url = "https://github.com/carlthome/rustler";
+    rev = "2861e2df25a84f2b49f6e5d12ccb10087f03aaf0";
   };
-  nativeBuildInputs = [ pkgs.pkg-config ];
-  buildInputs =
-    with pkgs;
-    pkgs.lib.optionals pkgs.stdenv.isLinux [
-      glib
-      gtk3
-      cairo
-      pango
-      gdk-pixbuf
-      glibc
-      xorg.libX11
-      xorg.libXcursor
-      xorg.libXrandr
-      xorg.libXi
-      xorg.libXext
-      xorg.libXinerama
-      xorg.libXxf86vm
-      xorg.libXrender
-      xorg.libxcb
-      xorg.libXau
-      xorg.libXdmcp
-      mesa
-      alsa-lib
-      dbus
-      freetype
-      fontconfig
-      zlib
-      udev
-    ];
-  postInstall = ''
-    cp -r resources $out/bin
-  '';
-}
+  package = import src { inherit pkgs; };
+in
+package
