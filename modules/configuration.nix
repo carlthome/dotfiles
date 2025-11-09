06cc423976971db2d5a67f2005a3cdbf0ac984d9
Carl Thomé <carlthome@gmail.com>
2025-11-09 19:02:56 +0100

    Add nix-community.cachix.org

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -8,11 +8,13 @@
     ];
     substituters = [
       "https://carlthome.cachix.org"
+      "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
       "https://devenv.cachix.org"
     ];
     trusted-public-keys = [
       "carlthome.cachix.org-1:BHerYg0J5Qv/Yw/SsxqPBlTY+cttA9axEsmrK24R15w="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
