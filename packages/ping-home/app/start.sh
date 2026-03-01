9d0bcb40d077d84cc313af8f25112d5e634d3339
Carl Thomé <carlthome@gmail.com>
2026-03-01 18:45:36 +0100

    Avoid having `uv` in runtime

--- packages/ping-home/app/start.sh
+++ packages/ping-home/app/start.sh
@@ -2,4 +2,4 @@
 tailscaled --tun=userspace-networking --socks5-server="${TS_SOCKS5_SERVER}" &
 sleep 2
 tailscale up --authkey="${TS_AUTHKEY}" --hostname="${TS_HOSTNAME}" --accept-routes
-exec uv run uvicorn --host 0.0.0.0 --port 8080 main:app
+exec uvicorn --host 0.0.0.0 --port 8080 main:app
