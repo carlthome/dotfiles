eb2ee147508bf0445d7a267c6a03784d9b869a8c
Carl Thomé <carlthome@gmail.com>
2026-03-01 21:11:02 +0100

    Remove flags and use grep to filter instead

--- packages/ping-home/app/start.sh
+++ packages/ping-home/app/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-TS_AUTHKEY=$(cat /secrets/tailscale-auth-key)
-tailscaled --tun=userspace-networking --socks5-server="${TS_SOCKS5_SERVER}" --statedir=/tmp/tailscale &
+
+# Start Tailscale in userspace networking mode with a SOCKS5 server.
+tailscaled --tun=userspace-networking --socks5-server="${TS_SOCKS5_SERVER}" &
 sleep 2
-tailscale up --quiet --authkey="${TS_AUTHKEY}" --hostname="${TS_HOSTNAME}" --accept-routes
+
+# Authenticate with Tailscale using the provided auth key.
+TS_AUTHKEY=$(cat /secrets/tailscale-auth-key)
+tailscale up --authkey="${TS_AUTHKEY}" --hostname="${TS_HOSTNAME}" --accept-routes 2>&1 | grep -i -E "error|warn|fatal"
+
+# Start the FastAPI application using Uvicorn.
 exec uvicorn --host 0.0.0.0 --port 8080 main:app
