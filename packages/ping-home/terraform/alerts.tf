648f534ec480b1bc278832472180598c26544c67
Carl Thomé <carlthome@gmail.com>
2026-03-01 19:30:50 +0100

    Add specific alert for tunneling issues

--- packages/ping-home/terraform/alerts.tf
+++ packages/ping-home/terraform/alerts.tf
@@ -24,3 +24,19 @@ resource "google_monitoring_alert_policy" "lan_down" {
     notification_rate_limit { period = "3600s" }
   }
 }
+
+resource "google_monitoring_alert_policy" "invalid_api_key_alert" {
+  display_name = "Cannot access Tailscale network"
+  combiner     = "OR"
+  severity     = "ERROR"
+  conditions {
+    display_name = "Logs contain invalid API key error"
+    condition_matched_log {
+      filter = "resource.type=\"cloud_run_revision\" AND resource.labels.service_name=\"${google_cloud_run_v2_service.checker.name}\" AND textPayload:\"invalid key: API key\""
+    }
+  }
+  notification_channels = [google_monitoring_notification_channel.email.name]
+  alert_strategy {
+    notification_rate_limit { period = "3600s" }
+  }
+}
