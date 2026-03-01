2f99604eaa0c3bb6fa6db28f2e903350977fb6b5
Carl Thomé <carlthome@gmail.com>
2026-03-01 20:56:40 +0100

    Reduce logging

--- packages/ping-home/app/start.sh
+++ packages/ping-home/app/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TS_AUTHKEY=$(cat /secrets/tailscale-auth-key)
-tailscaled --tun=userspace-networking --socks5-server="${TS_SOCKS5_SERVER}" &
+tailscaled --tun=userspace-networking --socks5-server="${TS_SOCKS5_SERVER}" --statedir=/tmp/tailscale &
 sleep 2
-tailscale up --authkey="${TS_AUTHKEY}" --hostname="${TS_HOSTNAME}" --accept-routes
+tailscale up --quiet --authkey="${TS_AUTHKEY}" --hostname="${TS_HOSTNAME}" --accept-routes
 exec uvicorn --host 0.0.0.0 --port 8080 main:app
