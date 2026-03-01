589e6fa4b1db6c61e36c382dc2121b677a501251
Carl Thomé <carlthome@gmail.com>
2026-03-02 00:42:36 +0100

    Apply Cloud Run revision to Tailscale hostname

--- packages/ping-home/app/start.sh
+++ packages/ping-home/app/start.sh
@@ -4,6 +4,11 @@
 tailscaled --tun=userspace-networking --socks5-server="${TS_SOCKS5_SERVER}" 2>&1 | grep -i -E "error|warn|fatal" &
 sleep 2
 
+# Append Cloud Run revision to hostname.
+if [ -n "${K_REVISION}" ]; then
+	TS_HOSTNAME="${TS_HOSTNAME}-${K_REVISION}"
+fi
+
 # Authenticate with Tailscale using the provided auth key.
 TS_AUTHKEY=$(cat /secrets/tailscale-auth-key)
 tailscale up --authkey="${TS_AUTHKEY}" --hostname="${TS_HOSTNAME}" --accept-routes 2>&1 | grep -i -E "error|warn|fatal"
