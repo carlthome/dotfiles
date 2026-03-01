33ee60e3d8ed63b340042d1051fe0231c31c47b3
Carl Thomé <carlthome@gmail.com>
2026-03-01 19:46:03 +0100

    Get value from mount

--- packages/ping-home/app/start.sh
+++ packages/ping-home/app/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+TS_AUTHKEY=$(cat /secrets/tailscale-auth-key)
 tailscaled --tun=userspace-networking --socks5-server="${TS_SOCKS5_SERVER}" &
 sleep 2
 tailscale up --authkey="${TS_AUTHKEY}" --hostname="${TS_HOSTNAME}" --accept-routes
