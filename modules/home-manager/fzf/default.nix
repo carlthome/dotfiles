34a9d0e75a26d34b4f629d1215c6ebdb0c6ac6aa
Carl Thomé <carlthome@gmail.com>
2026-03-22 23:59:07 +0100

    Add fzf-preview and fzf-git

--- modules/home-manager/fzf/default.nix
+++ modules/home-manager/fzf/default.nix
@@ -8,67 +8,81 @@
 
 let
   system = pkgs.stdenv.hostPlatform.system;
+  fzf-rg = lib.getExe self.packages.${system}.fzf-ripgrep;
 
-  fzfFunctions = {
-    fzf-rg = {
-      cmd = lib.getExe self.packages.${system}.fzf-ripgrep;
-      zshKey = "^F";
-      bashKey = "\\C-f";
-      fishKey = "\\cf";
-    };
-  };
+  fzf-preview =
+    let
+      src = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/junegunn/fzf/master/bin/fzf-preview.sh";
+        hash = "sha256-DkoKSJtUfap13Y7u/WtShgZ3QBsrd1T938txoYZdRVI=";
+        executable = true;
+      };
+    in
+    pkgs.writeShellScriptBin "fzf-preview.sh" ''
+      export PATH="${
+        lib.makeBinPath [
+          pkgs.bat
+          pkgs.file
+          pkgs.chafa
+          pkgs.coreutils
+        ]
+      }:$PATH"
+      exec ${src} "$@"
+    '';
 
-  zshInit = builtins.concatStringsSep "\n\n" (
-    builtins.attrValues (
-      builtins.mapAttrs (
-        name:
-        { cmd, zshKey, ... }:
-        ''
-          ${name}() {
-            ${cmd} "$BUFFER"
-            zle reset-prompt
-          }
-          zle -N ${name}
-          bindkey '${zshKey}' ${name}
-        ''
-      ) fzfFunctions
-    )
-  );
+  # Define FZF options here so they can be exported in shell init (avoids session var caching)
+  fzfDefaultOpts = [
+    "--height 100%"
+    "--style full"
+  ];
+  fzfCtrlTCommand = "fd --type f";
+  fzfCtrlTOpts = [
+    "--preview '${lib.getExe fzf-preview} {}'"
+    "--bind 'focus:transform-header:file --brief {}'"
+  ];
 
-  bashInit = builtins.concatStringsSep "\n\n" (
-    builtins.attrValues (
-      builtins.mapAttrs (
-        name:
-        { cmd, bashKey, ... }:
-        ''
-          ${name}() {
-            ${cmd} "$READLINE_LINE"
-          }
-          bind -x '"${bashKey}":${name}'
-        ''
-      ) fzfFunctions
-    )
-  );
+  toOpts = lib.concatStringsSep " ";
 in
 {
   programs.fzf = {
     enable = true;
     tmux.enableShellIntegration = config.programs.tmux.enable;
-    defaultOptions = [ "--height 100%" ];
-    fileWidgetOptions = [
-      "--preview 'stat {}'"
-      "--preview-window noborder"
-    ];
+    defaultOptions = fzfDefaultOpts;
+    fileWidgetCommand = fzfCtrlTCommand;
+    fileWidgetOptions = fzfCtrlTOpts;
   };
 
-  home.packages = with pkgs; [
-    fzf-git-sh
-  ];
+  programs.zsh.initContent = ''
+    export FZF_DEFAULT_OPTS="${toOpts fzfDefaultOpts}"
+    export FZF_CTRL_T_COMMAND="${fzfCtrlTCommand}"
+    export FZF_CTRL_T_OPTS="${toOpts fzfCtrlTOpts}"
 
-  home.shellAliases = {
-    "." = lib.getExe self.packages.${system}.fzf-open;
-  };
+    fzf-rg() { ${fzf-rg} "$BUFFER"; zle reset-prompt; }
+    zle -N fzf-rg
+    bindkey '^F' fzf-rg
+    source ${pkgs.fzf-git-sh}/share/fzf-git-sh/fzf-git.sh
 
-  programs.zsh.initContent = zshInit;
-  programs.bash.initExtra = bashInit;
+  '';
+
+  programs.bash.initExtra = ''
+    export FZF_DEFAULT_OPTS="${toOpts fzfDefaultOpts}"
+    export FZF_CTRL_T_COMMAND="${fzfCtrlTCommand}"
+    export FZF_CTRL_T_OPTS="${toOpts fzfCtrlTOpts}"
+
+    fzf-rg() { ${fzf-rg} "$READLINE_LINE"; }
+    bind -x '"\C-f":fzf-rg'
+    source ${pkgs.fzf-git-sh}/share/fzf-git-sh/fzf-git.sh
+
+  '';
+
+  programs.fish.interactiveShellInit = ''
+    set -gx FZF_DEFAULT_OPTS "${toOpts fzfDefaultOpts}"
+    set -gx FZF_CTRL_T_COMMAND "${fzfCtrlTCommand}"
+    set -gx FZF_CTRL_T_OPTS "${toOpts fzfCtrlTOpts}"
+
+    function fzf-rg; ${fzf-rg} (commandline); commandline -f repaint; end
+    bind \cf fzf-rg
+    source ${pkgs.fzf-git-sh}/share/fzf-git-sh/fzf-git.fish
+
+  '';
 }
