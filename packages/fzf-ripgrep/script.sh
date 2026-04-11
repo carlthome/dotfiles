beeccee5d832dc21dc04714b99c3d72dd6046e97
Carl Thomé <carlthome@gmail.com>
2026-04-11 12:25:35 +0200

    Toggle between ripgrep and fzf, and files or lines

--- packages/fzf-ripgrep/script.sh
+++ packages/fzf-ripgrep/script.sh
@@ -1,45 +1,45 @@
 #!/usr/bin/env bash
+# Interactive ripgrep + fzf for large codebases
+# - Lazy execution: ripgrep runs on-demand as you type
+# - Mode switching: CTRL-T toggles between ripgrep and fzf filtering
+# - View switching: CTRL-G toggles between lines and files view
 
-# 1. Search for text in files using Ripgrep
-# 2. Interactively narrow down the list using fzf
-# 3. Open the file in Vim
-rg --color=always --line-number --no-heading --smart-case "${*:-}" |
-	fzf --ansi \
-		--color "hl:-1:underline,hl+:-1:underline:reverse" \
-		--delimiter : \
-		--preview 'bat --color=always {1} --highlight-line {2}' \
-		--preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
-		--bind 'enter:become(vim {1} +{2})'
+INITIAL_QUERY="${*:-}"
+RG_LINES="rg --line-number --no-heading --color=always --smart-case"
+RG_FILES="rg --files-with-matches --color=always --smart-case"
 
-# TODO Use the following if working directory contains many files.
+# Create temp directory for state
+STATE_DIR="${TMPDIR:-/tmp}/rg-fzf.$$"
+mkdir -p "$STATE_DIR"
+trap 'rm -rf "$STATE_DIR"' EXIT
 
-# # 1. Search for text in files using Ripgrep
-# # 2. Interactively restart Ripgrep with reload action
-# # 3. Open the file in Vim
-# RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
-# INITIAL_QUERY="${*:-}"
-# : | fzf --ansi --disabled --query "$INITIAL_QUERY" \
-#     --bind "start:reload:$RG_PREFIX {q}" \
-#     --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
-#     --delimiter : \
-#     --preview 'bat --color=always {1} --highlight-line {2}' \
-#     --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
-#     --bind 'enter:become(vim {1} +{2})'
+RG_QUERY="$STATE_DIR/rg-query"
+FZF_QUERY="$STATE_DIR/fzf-query"
 
-# # Switch between Ripgrep mode and fzf filtering mode (CTRL-T)
-# rm -f /tmp/rg-fzf-{r,f}
-# RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
-# INITIAL_QUERY="${*:-}"
-# : | fzf --ansi --disabled --query "$INITIAL_QUERY" \
-#     --bind "start:reload:$RG_PREFIX {q}" \
-#     --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
-#     --bind 'ctrl-t:transform:[[ ! $FZF_PROMPT =~ ripgrep ]] &&
-#       echo "rebind(change)+change-prompt(1. ripgrep> )+disable-search+transform-query:echo \{q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r" ||
-#       echo "unbind(change)+change-prompt(2. fzf> )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"' \
-#     --color "hl:-1:underline,hl+:-1:underline:reverse" \
-#     --prompt '1. ripgrep> ' \
-#     --delimiter : \
-#     --header 'CTRL-T: Switch between ripgrep/fzf' \
-#     --preview 'bat --color=always {1} --highlight-line {2}' \
-#     --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
-#     --bind 'enter:become(vim {1} +{2})'
+fzf --ansi --disabled --query "$INITIAL_QUERY" \
+	--prompt 'rg lines> ' \
+	--header '^T: rg/fzf | ^G: lines/files | ?: preview' \
+	--color "hl:-1:underline,hl+:-1:underline:reverse" \
+	--delimiter : \
+	--bind "start:reload:$RG_LINES {q}" \
+	--bind "change:reload:sleep 0.1; $RG_LINES {q} || true" \
+	--bind "ctrl-t:transform:
+      if [[ \$FZF_PROMPT =~ rg ]]; then
+        [[ \$FZF_PROMPT =~ files ]] && p='fzf files> ' || p='fzf lines> ';
+        echo \"unbind(change)+change-prompt(\$p)+enable-search+transform-query:echo {q} > $RG_QUERY; cat $FZF_QUERY\"
+      else
+        [[ \$FZF_PROMPT =~ files ]] && p='rg files> ' || p='rg lines> ';
+        echo \"rebind(change)+change-prompt(\$p)+disable-search+transform-query:echo {q} > $FZF_QUERY; cat $RG_QUERY\"
+      fi" \
+	--bind "ctrl-g:transform:
+      [[ \$FZF_PROMPT =~ rg ]] && m=rg || m=fzf;
+      if [[ \$FZF_PROMPT =~ files ]]; then
+        echo \"change-prompt(\$m lines> )+reload($RG_LINES {q})\"
+      else
+        echo \"change-prompt(\$m files> )+reload($RG_FILES {q})\"
+      fi" \
+	--preview 'BAT_THEME=ansi bat --color=always {1} --highlight-line {2} --style=plain' \
+	--preview-window '+{2}+3/3,~3' \
+	--bind 'focus:transform-preview-label:echo {1}' \
+	--bind '?:toggle-preview' \
+	--bind 'enter:become(vim {1} +{2})'
