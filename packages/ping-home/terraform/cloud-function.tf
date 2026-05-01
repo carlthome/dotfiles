674e3f6d4ced79317a98ce5a43619ad7a112ccc0
Carl Thomé <carlthome@gmail.com>
2026-05-01 11:42:22 +0200

    Add alert policy

---
+++ packages/ping-home/terraform/cloud-function.tf
@@ -0,0 +1,37 @@
+resource "google_service_account" "heartbeat" {
+  account_id   = "heartbeat-function"
+  display_name = "Heartbeat Cloud Function"
+}
+
+resource "google_project_iam_member" "heartbeat_metrics" {
+  project = var.google_project
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.heartbeat.email}"
+}
+
+resource "google_monitoring_alert_policy" "heartbeat_missing" {
+  display_name = "Home heartbeat missing"
+  combiner     = "OR"
+  severity     = "CRITICAL"
+
+  conditions {
+    display_name = "No heartbeat for 10 minutes"
+    condition_absent {
+      filter   = "resource.type = \"global\" AND metric.type = \"custom.googleapis.com/home/heartbeat\""
+      duration = "600s"
+      aggregations {
+        alignment_period   = "300s"
+        per_series_aligner = "ALIGN_COUNT"
+      }
+    }
+  }
+
+  notification_channels = [google_monitoring_notification_channel.email.name]
+  alert_strategy {
+    notification_rate_limit { period = "3600s" }
+  }
+}
+
+output "heartbeat_service_account" {
+  value = google_service_account.heartbeat.email
+}
