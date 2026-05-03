38cf8c52a1ecb8a354964862498788c20a0b487b
Carl Thomé <carlthome@gmail.com>
2026-05-03 17:42:34 +0200

    Set PYTORCH_MPS_HIGH_WATERMARK_RATIO instead

--- overlays/accelerate-mps-fix.nix
+++ overlays/accelerate-mps-fix.nix
@@ -1,10 +1,12 @@
+# Fix accelerate tests failing on macOS CI runners with limited GPU memory
 final: prev: {
   python3Packages = prev.python3Packages.override {
     overrides = pfinal: pprev: {
       accelerate = pprev.accelerate.overridePythonAttrs (old: {
-        disabledTests = (old.disabledTests or [ ]) ++ [
-          "test_mps"
-        ];
+        preCheck = (old.preCheck or "") + ''
+          # Disable MPS memory limit on CI runners with limited GPU memory
+          export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
+        '';
       });
     };
   };
