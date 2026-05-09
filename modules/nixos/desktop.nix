02e977358b62324f41c6aa5d5d52cb850689e3e4
Carl Thomé <carlthome@gmail.com>
2026-05-09 12:48:08 +0200

    Add Plasma 6

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -9,6 +9,7 @@
   # Install GNOME and COSMIC desktop environments.
   services.desktopManager.gnome.enable = true;
   services.desktopManager.cosmic.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Enable lock screen by default.
   programs.dconf.profiles.user.databases = [
