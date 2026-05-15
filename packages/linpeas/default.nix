a23324a4f737113848c912d3c3481eb0f03f8bb4
Carl Thomé <carlthome@gmail.com>
2026-05-16 01:07:13 +0200

    Bump script

--- packages/linpeas/default.nix
+++ packages/linpeas/default.nix
@@ -8,6 +8,6 @@
 writeScriptBin "linpeas" (
   builtins.readFile (fetchurl {
     url = "https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh";
-    hash = "sha256-6GifZASmb9VraNT1DybbgP1VhKHWULRsIg5pwJEppes=";
+    hash = "sha256-0pCcSlBpBhgVIzRNmjNoBlftUIZj0gRLXvj7haKg/D4=";
   })
 )
