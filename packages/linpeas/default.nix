37d3f17cef35765b099dbcd79edbcf6e95e42110
Carl Thomé <carlthome@gmail.com>
2026-05-16 01:12:28 +0200

    Avoid IFD

--- packages/linpeas/default.nix
+++ packages/linpeas/default.nix
@@ -1,13 +1,19 @@
 {
-  lib,
-  writeScriptBin,
+  stdenv,
   fetchurl,
   ...
 }:
 
-writeScriptBin "linpeas" (
-  builtins.readFile (fetchurl {
+stdenv.mkDerivation {
+  name = "linpeas";
+  src = fetchurl {
     url = "https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh";
     hash = "sha256-0pCcSlBpBhgVIzRNmjNoBlftUIZj0gRLXvj7haKg/D4=";
-  })
-)
+  };
+  dontUnpack = true;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/linpeas
+    chmod +x $out/bin/linpeas
+  '';
+}
