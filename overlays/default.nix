8f4ec6e311bf32df5bf2dd109a613d1d398f7d46
Carl Thomé <carlthome@gmail.com>
2026-07-04 10:32:29 +0200

    Expose patch overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,7 @@
 { nixpkgs-unstable, ... }:
 {
   nixpkgs-unstable = import ./nixpkgs-unstable.nix { inherit nixpkgs-unstable; };
+  nixpkgs-patches = import ./nixpkgs-patches.nix;
   modules-closure = import ./modules-closure.nix { };
   github-actions = import ./github-actions.nix;
 }
