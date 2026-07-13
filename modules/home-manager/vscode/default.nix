61983bc7ae13da84bca52c1fa8962adf98f890f6
Carl Thomé <carlthome@gmail.com>
2026-07-13 10:05:28 +0200

    Flip between Visual Studio Code and VSCodium by allowVscode helper function

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -7,20 +7,26 @@
 }:
 let
   system = pkgs.stdenv.hostPlatform.system;
+
+  allowVscode =
+    (pkgs.config.allowUnfree or false)
+    || (pkgs.config ? allowUnfreePredicate && pkgs.config.allowUnfreePredicate "vscode");
+
   settings-directory =
     if pkgs.stdenv.hostPlatform.isDarwin then
       "$HOME/Library/Application Support/Code/User"
     else
       "$HOME/.config/Code/User";
+
   defaultExtensions = {
     "remote.SSH.defaultExtensions" = map (x: x.vscodeExtUniqueId) extensions;
   };
+
   userSettings = (builtins.fromJSON (builtins.readFile ./settings.json)) // defaultExtensions;
   keybindings = builtins.fromJSON (builtins.readFile ./keybindings.json);
   userTasks = builtins.fromJSON (builtins.readFile ./tasks.json);
-
   extensions = import ./extensions.nix {
-    inherit pkgs;
+    inherit pkgs allowVscode;
     vscode-extensions = nix-vscode-extensions.extensions.${system};
   };
 in
@@ -38,6 +44,6 @@ in
       enableUpdateCheck = false;
     };
     mutableExtensionsDir = false;
-    package = if pkgs.config.allowUnfreePredicate "vscode" then pkgs.vscode else pkgs.vscodium;
+    package = if allowVscode then pkgs.vscode else pkgs.vscodium;
   };
 }
--- modules/home-manager/vscode/extensions.nix
+++ modules/home-manager/vscode/extensions.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   vscode-extensions,
+  allowVscode,
   ...
 }:
 
@@ -33,7 +34,7 @@ let
     ms-python.mypy-type-checker
     ms-python.pylint
     ms-python.python
-    (if pkgs.config.allowUnfreePredicate "vscode" then ms-python.vscode-pylance else ms-pyright.pyright)
+    (if allowVscode then ms-python.vscode-pylance else ms-pyright.pyright)
     ms-toolsai.datawrangler
     ms-toolsai.jupyter
     ms-toolsai.jupyter-keymap
