29d17561541efe3bcb484aa7bc74c794121a5fd8
Carl Thomé <carlthome@gmail.com>
2026-08-10 23:40:25 +0200

    Bump linpeas script

--- packages/linpeas/default.nix
+++ packages/linpeas/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
   name = "linpeas";
   src = fetchurl {
     url = "https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh";
-    hash = "sha256-iL4ndh7JTHX00UIIq0MaS9wY5V81IfLzOwkuwl5K/mU=";
+    hash = "sha256-Dqfpzl/MpGS1mYy3OTDjZkfr+bOFkP4lC71WZP6GcKU=";
   };
   dontUnpack = true;
   installPhase = ''
